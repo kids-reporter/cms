adb60980a0905ae1c9ee261c02cba13940825a1d
duidae <duidae@gmail.com>
2023-07-13 15:34:20 +0800

    feat: test applying css on post intro

--- packages/frontend/src/app/[slug]/page.tsx
+++ packages/frontend/src/app/[slug]/page.tsx
@@ -18,6 +18,8 @@ async function fetchArticle() {
 // 1. import draft from draft-renderer
 // 2. prepare mockup for display
 
+import './post.css'
+
 export default async function Article({
   params,
 }: {
@@ -26,9 +28,18 @@ export default async function Article({
   // TODO: fetch data from graphQL of keystone
   // const data = await fetchArticle()
   return (
-    <>
+    <div className="post">
       <h1>Article slug: {params.slug}</h1>
+      <div className="post-intro">
+        <p>
+          台灣大學「探索學習」課程打破了學習場域與修課的界限，讓學習不再只限於校內。學生得以運用校內及外部資源，自行制定學習內容、也能拿到課堂學分。學生透過探索計畫找到學習方向、甚至尋回學習動機。
+        </p>
+        <p>
+          本文作者張恩瑋喜愛動物，2022年參與探索學習課程，她便選擇探索「動物園」產業，推助她從農業化學系轉系到動物科學技術學系，申請上創新領域學士學位學程。
+        </p>
+        {/*<p><span style="color: #575757"><code>（文字／<span style="text-decoration: underline"><a style="color: #575757;text-decoration: underline" href="https://kids.twreporter.org/staff/chang-en-wei/" target="_blank" rel="noopener">張恩瑋</a></span>；設計／<span style="text-decoration: underline"><a style="color: #575757;text-decoration: underline" href="https://kids.twreporter.org/staff/wang-chia-chen/" target="_blank" rel="noopener">王家琛</a></span>、<span style="text-decoration: underline"><a style="color: #575757;text-decoration: underline" href="https://kids.twreporter.org/staff/hychen/" target="_blank" rel="noopener">黃禹禛</a></span>；核稿／<span style="text-decoration: underline"><a style="color: #575757;text-decoration: underline" href="https://kids.twreporter.org/staff/jill718/" target="_blank" rel="noopener">楊惠君</a></span>；責任編輯／<span style="text-decoration: underline"><a style="color: #575757;text-decoration: underline" href="https://kids.twreporter.org/staff/yunruchen/" target="_blank" rel="noopener">陳韻如</a></span>）</code></span></p>*/}
+      </div>
       {/* TODO: draftRenderer */}
-    </>
+    </div>
   )
 }
