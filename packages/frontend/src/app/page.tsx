ff74c316cde81360e14b59a0dbc301d67b1836e0
duidae <duidae@gmail.com>
2023-07-14 17:32:32 +0800

    fix: add slug mockup to home

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,9 +1,24 @@
 // import styles from './page.module.css'
 
+// TODO: fetch from graphQL
+const articles = [
+  'metoo-body-boundary-law',
+  'university-exploratory-learning-zoo',
+]
+
 export default function Home() {
   return (
     <main>
       <h1>少年報導者首頁</h1>
+      {articles.map((article, index) => {
+        return (
+          <div key={`article-${index}`}>
+            <a href={`https://dev-kids.twreporter.org/${article}`}>
+              https://dev-kids.twreporter.org/{article}
+            </a>
+          </div>
+        )
+      })}
     </main>
   )
 }
