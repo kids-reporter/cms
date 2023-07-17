46d9115e9c8688f41c790591d748cefc9cf531e0
duidae <duidae@gmail.com>
2023-07-17 16:19:06 +0800

    fix: refine a little bit

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -6,7 +6,7 @@ type Post = {
   slug: string
 }
 
-const siteURL = 'https://dev-kids.twreporter.org'
+const siteURL = 'http://localhost:3000' // 'https://dev-kids.twreporter.org'
 const apiURL = 'https://dev-kids-cms.twreporter.org/api/graphql'
 
 export default async function Home() {
@@ -20,15 +20,18 @@ export default async function Home() {
     }
   `,
   })
-  const posts: Post[] = response.data.data.posts
+  const posts: Post[] = response?.data?.data?.posts
+  console.log(response, posts)
 
   return (
     <main>
       <h1>少年報導者首頁</h1>
-      {posts.map((post, index) => {
+      {posts?.map((post, index) => {
         return (
           <div key={`article-${index}`}>
             <a href={`${siteURL}/${post.slug}`}>{post.name}</a>
+            <p>{post.slug}</p>
+            <br />
           </div>
         )
       })}
