da145767fbed2d637bb1e9383599f60e4517f979
duidae <duidae@gmail.com>
2023-07-18 18:34:23 +0800

    fix: add editors to brief

---
+++ packages/frontend/src/app/[slug]/editors.tsx
@@ -0,0 +1,56 @@
+export type EditorGroup = {
+  title: string
+  editors: {
+    name: string
+    link: string
+  }[]
+}
+
+type EditorsProp = {
+  editorGroups: EditorGroup[]
+}
+
+export const Editors = (props: EditorsProp) => {
+  return (
+    <p>
+      <span style={{ color: '#575757' }}>
+        {'('}
+        {props?.editorGroups?.map((editorGroup, editorGroupIndex) => {
+          return (
+            <>
+              {`${editorGroup.title}／`}
+              {editorGroup?.editors?.map((editor, index) => {
+                return (
+                  <>
+                    <span
+                      key={`editorGroup-${editorGroupIndex}`}
+                      style={{ textDecoration: 'underline' }}
+                    >
+                      <a
+                        key={`editor-${index}`}
+                        style={{
+                          color: '#575757',
+                          textDecoration: 'underline',
+                        }}
+                        href={editor.link}
+                        target="_blank"
+                        rel="noopener"
+                      >
+                        {editor.name}
+                      </a>
+                    </span>
+                    {index + 1 < editorGroup.editors.length ? '、' : ''}
+                  </>
+                )
+              })}
+              {editorGroupIndex + 1 < props.editorGroups.length ? `；` : ''}
+            </>
+          )
+        })}
+        {')'}
+      </span>
+    </p>
+  )
+}
+
+export default Editors
--- packages/frontend/src/app/[slug]/page.tsx
+++ packages/frontend/src/app/[slug]/page.tsx
@@ -12,6 +12,50 @@ import '../assets/css/dot-hr.css'
 import '../assets/css/icomoon/style.css'
 
 const apiURL = 'https://dev-kids-cms.twreporter.org/api/graphql'
+
+// mockups
+const editorsMockup = [
+  {
+    title: '文字',
+    editors: [
+      {
+        name: '張恩瑋',
+        link: 'https://kids.twreporter.org/staff/chang-en-wei/',
+      },
+    ],
+  },
+  {
+    title: '設計',
+    editors: [
+      {
+        name: '王家琛',
+        link: 'https://kids.twreporter.org/staff/wang-chia-chen/',
+      },
+      {
+        name: '黃禹禛',
+        link: 'https://kids.twreporter.org/staff/hychen/',
+      },
+    ],
+  },
+  {
+    title: '核稿',
+    editors: [
+      {
+        name: '楊惠君',
+        link: 'https://kids.twreporter.org/staff/jill718/',
+      },
+    ],
+  },
+  {
+    title: '責任編輯',
+    editors: [
+      {
+        name: '陳韻如',
+        link: 'https://kids.twreporter.org/staff/yunruchen/',
+      },
+    ],
+  },
+]
 const tagsMockup = [
   {
     link:
@@ -55,6 +99,7 @@ export default async function PostPage({
   })
   const post = response?.data?.data?.post
   post.tags = tagsMockup // TODO: find tags source
+  post.editors = editorsMockup // TODO: find editors source
 
   return (
     post && (
@@ -73,13 +118,13 @@ export default async function PostPage({
             </div>
           </header>
         </div>
-        <Brief content={post.brief} />
+        <Brief content={post.brief} editors={post.editors} />
         <h1>TODO: sidebar</h1>
         <Divider />
 
         {post && <PostRenderer post={post} />}
-
         <Tags tags={post.tags} />
+
         <h1>TODO: 誰幫我們完成這篇文章</h1>
         <h1>TODO: call to action</h1>
         <h1>TODO: 相關文章</h1>
