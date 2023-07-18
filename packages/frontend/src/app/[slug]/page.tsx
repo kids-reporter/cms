0f61369999949019cef00193adda1168d5bccd82
許守傑 Shou-Chieh Hsu (Jason) <duidae@gmail.com>
2023-07-18 15:36:41 +0800

    Jason/post dev (#25)
    * fix: remove comments
    
    * fix: add post page structure
    
    * feat: enable title/brief
    
    * fix: fix filename
    
    * fix: rename folder to snake case & fix lint

--- packages/frontend/src/app/[slug]/page.tsx
+++ packages/frontend/src/app/[slug]/page.tsx
@@ -1,13 +1,11 @@
 import axios from 'axios'
-import PostRenderer from './postRenderer'
+import PostRenderer from './post-renderer'
 
 import '../assets/css/post.css'
 import '../assets/css/dot-hr.css'
 import '../assets/css/icomoon/style.css'
 
 const apiURL = 'https://dev-kids-cms.twreporter.org/api/graphql'
-// TODO:
-// fetch data via server, render via client
 
 export default async function PostPage({
   params,
@@ -22,6 +20,10 @@ export default async function PostPage({
               name
               brief
               content
+              publishedDate
+              editors {
+                name
+              }
             }
           }
         `,
@@ -31,15 +33,59 @@ export default async function PostPage({
       },
     },
   })
+
+  const getFormattedDate = (date: string): string => {
+    const dateObj = new Date(date)
+    const year = dateObj?.getFullYear()
+    const month = `${dateObj?.getMonth() + 1}`.padStart(2, '0')
+    const day = `${dateObj?.getDay() + 1}`.padStart(2, '0')
+    return [year, month, day].join('.')
+  }
+
   const post = response?.data?.data?.post
-  console.log(post?.name)
+  const postDate = getFormattedDate(post?.publishedDate)
 
   return (
-    <div className="post">
-      <h1>Slug: {params.slug}</h1>
-      <h1>Title: {post?.name}</h1>
-      <hr className="dot-hr mt-18 mb-18" />
-      {post && <PostRenderer post={post} />}
-    </div>
+    post && (
+      <div className="post">
+        <h1>TODO: header</h1>
+        <h1>TODO: 首圖</h1>
+        <div className="hero-section" data-type="type-1">
+          <header className="entry-header">
+            <h1 className="page-title">{post.name}</h1>
+            <div className="post_date_category">
+              <div className="post_date">刊出日期 {postDate}</div>
+              <div className="post_primary_category">
+                <a
+                  href="/category/university-exploratory-learning-teaching"
+                  className="rpjr-btn rpjr-btn-theme"
+                >
+                  TODO: category
+                </a>
+              </div>
+            </div>
+          </header>
+        </div>
+        <div className="post-intro">
+          {post.brief?.blocks?.map(
+            (block: any, index: number) =>
+              block?.text && (
+                <p key={`brief-paragraph-${index}`}>{block.text}</p>
+              )
+          )}
+        </div>
+        <h1>TODO: brief author</h1>
+        <h1>TODO: sidebar</h1>
+
+        <hr className="dot-hr mt-18 mb-18" />
+
+        {post && <PostRenderer post={post} />}
+        <h1>TODO: 關鍵字</h1>
+        <h1>TODO: 誰幫我們完成這篇文章</h1>
+        <h1>TODO: call to action</h1>
+        <h1>TODO: 相關文章</h1>
+        <h1>TODO: footer</h1>
+      </div>
+    )
   )
 }
