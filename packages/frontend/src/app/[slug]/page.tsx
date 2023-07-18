01b591caec9b5cbd4a2aad8cd7000d887229010d
duidae <duidae@gmail.com>
2023-07-18 10:08:18 +0800

    fix: refine a little bit

--- packages/frontend/src/app/[slug]/page.tsx
+++ packages/frontend/src/app/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import PostBody from './postBody'
+import PostRenderer from './postRenderer'
 
 import '../assets/css/post.css'
 import '../assets/css/dot-hr.css'
@@ -7,11 +7,12 @@ import '../assets/css/icomoon/style.css'
 
 const apiURL = 'https://dev-kids-cms.twreporter.org/api/graphql'
 
-export default async function Article({
+export default async function PostPage({
   params,
 }: {
   params: { slug: string }
 }) {
+  // TODO: error handling(params.slug, post)
   const response = await axios.post(apiURL, {
     query: `
           query($where: PostWhereUniqueInput!) {
@@ -29,15 +30,14 @@ export default async function Article({
     },
   })
   const post = response?.data?.data?.post
-  // TODO: error handling
-  console.log(post.name)
+  console.log(post?.name)
 
   return (
     <div className="post">
-      <h1>Article slug: {params.slug}</h1>
-      <h1>Article title: {post?.name}</h1>
-      {post && <PostBody post={post} />}
+      <h1>Slug: {params.slug}</h1>
+      <h1>Title: {post?.name}</h1>
       <hr className="dot-hr mt-18 mb-18" />
+      {post && <PostRenderer post={post} />}
     </div>
   )
 }
