b1639f6fa9bafe471d2b7ca8cbb8ebd0c8dfc729
duidae <duidae@gmail.com>
2023-07-21 13:58:25 +0800

    fix: fix footer style

--- packages/frontend/src/app/[slug]/footer.tsx
+++ packages/frontend/src/app/[slug]/footer.tsx
@@ -193,8 +193,6 @@ export const Footer = () => {
         <div className="footer-top">
           <div className="footer-top__left">
             <picture className="footer-top__left-logo">
-              {/*<source media="(min-width: 768px)" srcset="https://kids.twreporter.org/wp-content/themes/blocksy-child/assets/img/footer-logo.svg"/>*/}
-              {/*<source media="(max-width: 767px)" srcset="https://kids.twreporter.org/wp-content/uploads/2022/10/LOGO.svg"/>*/}
               <img
                 src="https://kids.twreporter.org/wp-content/themes/blocksy-child/assets/img/footer-logo.svg"
                 alt=""
@@ -293,55 +291,7 @@ export const Footer = () => {
         </div>
       </div>
 
-      <div id="footer" className="ct-footer" data-id="type-1">
-        <div data-row="middle" className="ct-hidden-lg">
-          <div className="ct-container" data-columns-divider="md:sm">
-            <div data-column="text:uD_c5a">
-              <div
-                className="ct-header-text "
-                data-id="uD_c5a"
-                data-width="stretch"
-              >
-                <div className="entry-content">
-                  <p
-                    style={{
-                      fontSize: '14px',
-                      color: '#232323',
-                      padding: '0 10px',
-                    }}
-                  ></p>
-                  <p style={{ textAlign: 'center' }}>
-                    <a
-                      className="footer-link-m"
-                      style={{ textDecoration: 'none' }}
-                      href="https://www.twreporter.org/a/privacy-footer"
-                      target="_blank"
-                      rel="noopener"
-                    >
-                      <strong>隱私政策</strong>
-                    </a>{' '}
-                    <a
-                      className="footer-link-m"
-                      style={{ textDecoration: 'none' }}
-                      href="https://www.twreporter.org/a/license-footer"
-                      target="_blank"
-                      rel="noopener"
-                    >
-                      <strong>許可協議</strong>
-                    </a>
-                  </p>
-                  <p>&nbsp;</p>
-                  <p style={{ textAlign: 'center' }}>
-                    公益勸募許可字號｜衛部救字第 1101363853 號
-                  </p>
-                  <p style={{ textAlign: 'center' }}>
-                    Copyright © 2023 The Reporter
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div id="footer" className="footer-copyright" data-id="type-1">
         <div data-row="bottom" className="ct-hidden-sm ct-hidden-md">
           <div className="ct-container">
             <div data-column="text">
