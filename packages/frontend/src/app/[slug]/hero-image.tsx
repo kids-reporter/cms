50eab8ec4ad85d27a42ca4105e49ada3215ec808
duidae <duidae@gmail.com>
2023-07-25 10:15:20 +0800

    fix: fix hero image style

--- packages/frontend/src/app/[slug]/hero-image.tsx
+++ packages/frontend/src/app/[slug]/hero-image.tsx
@@ -9,7 +9,9 @@ export const HeroImage = (props: HeroImageProp) => {
   // TODO: image url?
   return (
     <figure className="hero-image">
-      <img src="https://kids.twreporter.org/wp-content/uploads/2023/07/%E5%BC%B5%E7%91%8B%E6%81%A9%E4%B8%BB%E5%9C%96.jpg" />
+      <div className="image-container">
+        <img src="https://kids.twreporter.org/wp-content/uploads/2023/07/%E5%BC%B5%E7%91%8B%E6%81%A9%E4%B8%BB%E5%9C%96.jpg" />
+      </div>
       <figcaption>{props.caption}</figcaption>
     </figure>
   )
