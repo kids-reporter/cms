6f3aa27446c0f52f70d3c4df54d53135f2b60c0a
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-07-26 16:30:32 +0800

    refactor(core)!: remove draftConverter

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,11 +1,9 @@
 import addManualOrderRelationshipFields from './utils/manual-order-relationship'
-import draftConverter from '@kids-reporter/draft-editor/lib/draft-converter'
 import { richTextEditor } from './custom-fields/rich-text-editor'
 
 export { buttonNames as richTextEditorButtonNames } from '@kids-reporter/draft-editor/lib/draft-editor'
 
 export const customFields = {
-  draftConverter,
   richTextEditor,
 }
 
