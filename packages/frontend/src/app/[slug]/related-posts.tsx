aaa718e1d84179bd3ab8193c6a7d2e297a93be61
許守傑 Shou-Chieh Hsu (Jason) <duidae@gmail.com>
2023-07-27 09:49:28 +0800

    Jason/post dev (#44)
    * fix: fix related post style
    
    * fix: fix slider style

--- packages/frontend/src/app/[slug]/related-posts.tsx
+++ packages/frontend/src/app/[slug]/related-posts.tsx
@@ -4,6 +4,8 @@ type RelatedPostsProp = {
   posts: any[]
 }
 
+const cmsURL = 'https://dev-kids-cms.twreporter.org'
+
 export const RelatedPosts = (props: RelatedPostsProp) => {
   return (
     props?.posts?.length > 0 && (
@@ -14,6 +16,30 @@ export const RelatedPosts = (props: RelatedPostsProp) => {
             alt="相關文章"
           />
         </h3>
+
+        <div className="related-post-slider">
+          <div className="cards">
+            {props.posts.map((post, index) => {
+              return (
+                <div key={`related-post-${index}`} className="post">
+                  <div>
+                    <img src={`${cmsURL}${post.heroImage?.imageFile?.url}`} />
+                  </div>
+                  {post.name}
+                  {post.publishedDate}
+                </div>
+              )
+            })}
+          </div>
+          <div className="bullets">
+            <button>&nbsp;</button>
+            <button className="active">&nbsp;</button>
+            <button>&nbsp;</button>
+            <button>&nbsp;</button>
+            <button>&nbsp;</button>
+            <button>&nbsp;</button>
+          </div>
+        </div>
       </div>
     )
   )
