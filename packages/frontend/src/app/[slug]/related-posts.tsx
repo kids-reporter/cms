3708fd20ea11515436308e20cc55c8e9a722f339
duidae <duidae@gmail.com>
2023-07-29 22:50:38 +0800

    fix: fix related post style

--- packages/frontend/src/app/[slug]/related-posts.tsx
+++ packages/frontend/src/app/[slug]/related-posts.tsx
@@ -1,3 +1,4 @@
+'use client'
 import './related-post.scss'
 
 type RelatedPostsProp = {
@@ -7,6 +8,13 @@ type RelatedPostsProp = {
 const cmsURL = 'https://dev-kids-cms.twreporter.org'
 
 export const RelatedPosts = (props: RelatedPostsProp) => {
+  const onPrevClick = () => {
+    console.log('prev')
+  }
+  const onNextClick = () => {
+    console.log('next')
+  }
+
   return (
     props?.posts?.length > 0 && (
       <div className="related-post-container">
@@ -25,11 +33,68 @@ export const RelatedPosts = (props: RelatedPostsProp) => {
                   <div>
                     <img src={`${cmsURL}${post.heroImage?.imageFile?.url}`} />
                   </div>
+                  {
+                    <a href="https://kids.twreporter.org/category/campus">
+                      校園寶可夢
+                    </a>
+                  }
                   {post.name}
+                  {
+                    '台灣學生閱讀文章的情感感受力較弱，為什麼會如此？文學作家朱宥勳以4個關鍵字，分享如何理解一篇抒情文，以及如何掏出真心，寫出一篇動人文章。'
+                  }
+                  {'動物'}
                   {post.publishedDate}
                 </div>
               )
             })}
+            <button onClick={onPrevClick}>
+              <svg
+                width="54"
+                height="54"
+                viewBox="0 0 54 54"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M27 2C13.1929 2 2 13.1929 2 27C2 40.8071 13.1929 52 27 52C40.8071 52 52 40.8071 52 27C52 13.1929 40.8071 2 27 2Z"
+                  fill="var(--theme-color)"
+                  stroke="white"
+                  stroke-width="3"
+                  stroke-miterlimit="10"
+                ></path>
+                <path
+                  d="M29.9297 39.1001L17.9359 27.0002L29.9297 14.9003"
+                  stroke="white"
+                  stroke-width="4"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+            </button>
+            <button onClick={onNextClick}>
+              <svg
+                width="54"
+                height="54"
+                viewBox="0 0 54 54"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M27 52C40.8071 52 52 40.8071 52 27C52 13.1929 40.8071 2 27 2C13.1929 2 2 13.1929 2 27C2 40.8071 13.1929 52 27 52Z"
+                  fill="var(--theme-color)"
+                  stroke="white"
+                  stroke-width="3"
+                  stroke-miterlimit="10"
+                ></path>
+                <path
+                  d="M24.0703 14.8999L36.0641 26.9998L24.0703 39.0997"
+                  stroke="white"
+                  stroke-width="4"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+            </button>
           </div>
           <div className="bullets">
             <button>&nbsp;</button>
