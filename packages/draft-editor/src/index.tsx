28444e055f0ec16538b1cf19789595c5ddeaf35a
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-08-04 16:44:23 +0800

    refactor(draft-editor): update src/index.tsx. Add named export

--- packages/draft-editor/src/index.tsx
+++ packages/draft-editor/src/index.tsx
@@ -1,6 +1,8 @@
 import { RichTextEditor, decorator } from './draft-editor'
 import buttonNames from './buttons/bt-names'
 
+export { RichTextEditor, buttonNames, decorator }
+
 export default {
   RichTextEditor,
   buttonNames,
