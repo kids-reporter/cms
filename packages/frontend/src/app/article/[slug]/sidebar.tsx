686fc4b2a9fe52ae388da870101220974a13867a
duidae <duidae@gmail.com>
2023-08-07 23:06:10 +0800

    fix: add sidebar functionality

--- packages/frontend/src/app/article/[slug]/sidebar.tsx
+++ packages/frontend/src/app/article/[slug]/sidebar.tsx
@@ -1,15 +1,63 @@
+'use client'
 import './sidebar.scss'
 
 const shareIcons = [
-  'rpjr-icon-color-fb.svg',
-  'rpjr-icon-color-twitter.svg',
-  'rpjr-icon-color-line.svg',
-  'rpjr-icon-color-link.svg',
+  {
+    image: 'rpjr-icon-color-fb.svg',
+    onClick: () => {
+      const currentURL = window.location.href
+      const location =
+        'https://www.facebook.com/sharer/sharer.php?' +
+        `u=${encodeURIComponent(currentURL)}`
+      window.open(location, '_blank')
+    },
+  },
+  {
+    image: 'rpjr-icon-color-twitter.svg',
+    onClick: () => {
+      const currentURL = window.location.href
+      const location =
+        'https://twitter.com/intent/tweet?' +
+        `url=${encodeURIComponent(currentURL)}&text=${encodeURIComponent(
+          document.title + ' #報導者'
+        )}`
+      window.open(location, '_blank')
+    },
+  },
+  {
+    image: 'rpjr-icon-color-line.svg',
+    onClick: () => {
+      const currentURL = window.location.href
+      const location = `https://social-plugins.line.me/lineit/share?url=${encodeURIComponent(
+        currentURL
+      )}`
+      window.open(location, '_blank')
+    },
+  },
+  {
+    image: 'rpjr-icon-color-link.svg',
+    onClick: () => {
+      const currentURL = window.location.href
+      navigator.clipboard.writeText(currentURL).then(() => {
+        window.alert('已複製文章網址')
+      })
+    },
+  },
 ]
 
-const functionIcons = ['rpjr-icon-color-text.svg', 'rpjr-icon-color-print.svg']
+const functionIcons = [
+  {
+    image: 'rpjr-icon-color-text.svg',
+    onClick: () => {
+      console.log('change font size')
+    }, // TODO: change font handler
+  },
+  {
+    image: 'rpjr-icon-color-print.svg',
+    onClick: () => window.print(),
+  },
+]
 
-// TODO: add functionality to buttons
 export const Sidebar = () => {
   return (
     <div className="sidebar-container">
@@ -18,8 +66,8 @@ export const Sidebar = () => {
           <span>分享</span>
           {shareIcons.map((icon, index) => {
             return (
-              <button key={`share-icon-${index}`}>
-                <img alt="" src={`/images/${icon}`} />
+              <button key={`share-icon-${index}`} onClick={icon.onClick}>
+                <img src={`/images/${icon.image}`} />
               </button>
             )
           })}
@@ -27,8 +75,8 @@ export const Sidebar = () => {
         <div className="section">
           {functionIcons.map((icon, index) => {
             return (
-              <button key={`function-icon-${index}`}>
-                <img alt="" src={`/images/${icon}`} />
+              <button key={`function-icon-${index}`} onClick={icon.onClick}>
+                <img src={`/images/${icon.image}`} />
               </button>
             )
           })}
