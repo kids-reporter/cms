6ada60d963d381e7e46d5c213a51ab876aa6a06d
duidae <duidae@gmail.com>
2023-08-09 10:58:58 +0800

    feat: eanble analyzer

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   output: 'standalone',
 }
-
 module.exports = nextConfig
+
+/*
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
+*/
