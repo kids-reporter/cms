c6b80cbda0d3a7fcb910df08937404f57ef5f489
duidae <duidae@gmail.com>
2023-08-15 09:43:31 +0800

    fix: refine a little bit

--- packages/frontend/src/app/article/[slug]/title.tsx
+++ packages/frontend/src/app/article/[slug]/title.tsx
@@ -9,7 +9,7 @@ export const Title = (props: TitleProp) => {
   const subtitle = props.subtitle
   return (
     <>
-      {subtitle && <div className="subtitle">{props.subtitle}</div>}
+      {subtitle && <div className="subtitle">{subtitle}</div>}
       <h1 className={`title ${subtitle ? '' : 'noSubtitle'}`}>{props.text}</h1>
     </>
   )
