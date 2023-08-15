140c383ebe838130070a80e79809e94326287fe1
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-08-15 16:18:37 +0800

    feat(cms): add `authorsJSON` and `resolveInput` hook to deal with authors information

--- packages/cms/lists/post.ts
+++ packages/cms/lists/post.ts
@@ -4,6 +4,7 @@ import {
 } from '@kids-reporter/cms-core'
 import { graphql, list } from '@keystone-6/core'
 import {
+  json,
   virtual,
   relationship,
   timestamp,
@@ -57,6 +58,10 @@ const listConfigurations = list({
       ui: {
         hideCreate: true,
       },
+    }),
+    authorsJSON: json({
+      label: '作者列',
+      defaultValue: [],
     }),
     heroImage: relationship({
       label: '首圖',
@@ -167,6 +172,111 @@ const listConfigurations = list({
       delete: () => true,
     },
   },
-  hooks: {},
+  hooks: {
+    resolveInput: async ({ inputData, item, resolvedData, context }) => {
+      let authorsJSON: AuthorsJSON =
+        inputData?.authorsJSON || item?.authorsJSON || []
+      authorsJSON = resolveAuthorsJSON(authorsJSON)
+
+      // `authors` is a relationship field.
+      // Therefore, `authors` only store author id
+      const relationshipAuthors: RelationshipInput = inputData?.authors
+      if (relationshipAuthors) {
+        const disconnect = relationshipAuthors?.disconnect
+        // delete disconnected authors from `authorsJSON`
+        if (Array.isArray(disconnect) && disconnect.length > 0) {
+          disconnect.forEach(({ id }) => {
+            authorsJSON = authorsJSON.filter((item) => {
+              // if `item.id` is not existed,
+              // which means it is manually added by users,
+              // and then we don't filter this item out.
+              if (!item.id) {
+                return true
+              }
+              // filter out disconnected item
+              return item.id !== id
+            })
+          })
+        }
+
+        // add new connected authors into `authorsJSON`
+        const connect = relationshipAuthors?.connect
+        if (Array.isArray(connect) && connect.length > 0) {
+          const ids = connect.map(({ id }) => id)
+          // find author items via gql query
+          const items = await context.query.Author.findMany({
+            where: { id: { in: ids } },
+            query: 'id name',
+          })
+          items.forEach((item) => {
+            authorsJSON.push({
+              id: item.id,
+              name: item.name,
+              type: 'link',
+              role: heuristicallyPickRole(item.name),
+            })
+          })
+        }
+      }
+
+      resolvedData.authorsJSON = authorsJSON
+      return resolvedData
+    },
+  },
 })
+
+/**
+ * This function is used to resolve field `authorsJSON`.
+ * It filters out invalid data and
+ * adds missing properties, such as `type`.
+ */
+function resolveAuthorsJSON(authorsJSON: AuthorsJSON): AuthorsJSON {
+  return authorsJSON
+    .filter(
+      (item) =>
+        typeof item === 'object' &&
+        typeof item.name === 'string' &&
+        typeof item.role === 'string'
+    )
+    .map((item) => {
+      if (item.id) {
+        item.type = 'link'
+      } else {
+        item.type = 'string'
+      }
+      return item
+    })
+}
+
+function heuristicallyPickRole(authorName: string): string {
+  switch (authorName) {
+    case '陳韻如': {
+      return '責任編輯'
+    }
+    case '楊惠君': {
+      return '核稿'
+    }
+    case '黃禹禛':
+    case '鄭涵文': {
+      return '設計'
+    }
+    default:
+      return '文字'
+  }
+}
+
+type AuthorsJSON = {
+  id?: string
+  name: string
+  type?: 'link' | 'string'
+  role: string
+}[]
+
+type RelationshipInput =
+  | {
+      disconnect?: { id: string }[]
+      connect?: { id: string }[]
+    }
+  | undefined
+
 export default listConfigurations
