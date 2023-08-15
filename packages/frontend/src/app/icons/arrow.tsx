195bf64573560ffa661c2e62d77e2842daca28a8
duidae <duidae@gmail.com>
2023-08-15 15:57:06 +0800

    fix: remove warnings

--- packages/frontend/src/app/icons/arrow.tsx
+++ packages/frontend/src/app/icons/arrow.tsx
@@ -15,15 +15,15 @@ export const ArrowLeft = (props: ArrowProp) => {
         d="M27 2C13.1929 2 2 13.1929 2 27C2 40.8071 13.1929 52 27 52C40.8071 52 52 40.8071 52 27C52 13.1929 40.8071 2 27 2Z"
         fill={props.color}
         stroke="white"
-        stroke-width="3"
-        stroke-miterlimit="10"
+        strokeWidth="3"
+        strokeMiterlimit="10"
       ></path>
       <path
         d="M29.9297 39.1001L17.9359 27.0002L29.9297 14.9003"
         stroke="white"
-        stroke-width="4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></path>
     </svg>
   )
@@ -42,15 +42,15 @@ export const ArrowRight = (props: ArrowProp) => {
         d="M27 52C40.8071 52 52 40.8071 52 27C52 13.1929 40.8071 2 27 2C13.1929 2 2 13.1929 2 27C2 40.8071 13.1929 52 27 52Z"
         fill={props.color}
         stroke="white"
-        stroke-width="3"
-        stroke-miterlimit="10"
+        strokeWidth="3"
+        strokeMiterlimit="10"
       ></path>
       <path
         d="M24.0703 14.8999L36.0641 26.9998L24.0703 39.0997"
         stroke="white"
-        stroke-width="4"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="4"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></path>
     </svg>
   )
