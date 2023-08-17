3764b7360c97f627213c4c5335a120d104e1a017
duidae <duidae@gmail.com>
2023-08-17 15:43:47 +0800

    fix: fix header

--- packages/frontend/src/app/components/header.tsx
+++ packages/frontend/src/app/components/header.tsx
@@ -178,7 +178,7 @@ export const StickyHeader = () => {
   )
 
   return (
-    <div className="header">
+    <div className="sticky-header">
       <div className="header-container">
         <div className="left">{brand}</div>
         <div className="right">
@@ -205,11 +205,85 @@ export const StickyHeader = () => {
 }
 
 export const Header = () => {
-  const handleSearchInputChange = () => {
-    // TODO: handle search
-    console.log('handleSearchInputChange')
-  }
+  const navi = (
+    <nav className="header-menu-1" aria-label="頁首選單">
+      <ul className="menu" role="menubar">
+        {navItems.map((item, index) => {
+          return (
+            <li key={`header-nav-item-${index}`}>
+              <a href={item.link} className="ct-menu-link" role="menuitem">
+                {item.title}
+              </a>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+  )
 
+  return (
+    <div className="home-header">
+      <div className="left">
+        <img src="/images/navbar_pic.svg" />
+      </div>
+      <div className="center">
+        <a>
+          <img
+            src="/images/logo-full.svg"
+            alt="少年報導者 The Reporter for Kids"
+          />
+        </a>
+        {navi}
+      </div>
+      <div className="right">
+        <div>
+          <form
+            role="search"
+            method="get"
+            className="search-form"
+            action="https://kids.twreporter.org/"
+            aria-haspopup="listbox"
+          >
+            <input
+              type="search"
+              placeholder="搜尋更多新聞、議題"
+              value=""
+              name="s"
+              title="Search for..."
+              aria-label="Search for..."
+            />
+            <button
+              type="submit"
+              className="search-submit"
+              aria-label="搜尋按鈕"
+            >
+              <svg
+                className="ct-icon"
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+              >
+                <path d="M14.8,13.7L12,11c0.9-1.2,1.5-2.6,1.5-4.2c0-3.7-3-6.8-6.8-6.8S0,3,0,6.8s3,6.8,6.8,6.8c1.6,0,3.1-0.6,4.2-1.5l2.8,2.8c0.1,0.1,0.3,0.2,0.5,0.2s0.4-0.1,0.5-0.2C15.1,14.5,15.1,14,14.8,13.7zM1.5,6.8c0-2.9,2.4-5.2,5.2-5.2S12,3.9,12,6.8S9.6,12,6.8,12S1.5,9.6,1.5,6.8z"></path>
+              </svg>
+              <span data-loader="circles">
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
+            </button>
+            <input type="hidden" name="post_type" value="post" />
+          </form>
+        </div>
+        <div>
+          {contributeBtn}
+          {subscribeBtn}
+          {aboutUsBtn}
+        </div>
+      </div>
+    </div>
+  )
+
+  /*
   return (
     <div data-row="top:boxed" data-column-set="3">
       <div className="ct-container">
@@ -420,4 +494,5 @@ export const Header = () => {
       </div>
     </div>
   )
+  */
 }
