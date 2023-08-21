2d5c37257fe8e09846beb785ca9bd9ab6b553968
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-08-21 17:38:19 +0800

    revert: "style(dr): clear float for righ|left aligned image"
    This reverts commit 399c118353a1afd081bf2f3c77598fb2ad46e170.

--- packages/draft-renderer/src/block-renderers/image-block.tsx
+++ packages/draft-renderer/src/block-renderers/image-block.tsx
@@ -60,7 +60,7 @@ export function ImageBlock({ className = '', data }: ImageBlockProps) {
         srcSet={imgSrcSetArr.join(',')}
         sizes="(min-width: 1200px) 1000px, 100vw"
       />
-      {desc && <FigureCaption>{desc}</FigureCaption>}
+      <FigureCaption>{desc}</FigureCaption>
     </Figure>
   )
 
@@ -73,12 +73,6 @@ const ArticleBodyContainer = styled.div<{ $alignment?: string }>`
   /* reset browser default styles */
   figure {
     margin: 0;
-  }
-
-  &::after {
-    content: '';
-    display: table;
-    clear: both;
   }
 
   max-width: 100%;
@@ -97,31 +91,23 @@ const ArticleBodyContainer = styled.div<{ $alignment?: string }>`
       case 'paragraph-width':
         return `
           ${theme.breakpoint.xl} {
-            figure {
-              width: 700px;
-              margin-left: auto;
-              margin-right: auto;
-            }
+            max-width: 700px;
           }
         `
       case 'right':
         return `
           ${theme.breakpoint.xl} {
-            figure {
-              width: 361px;
-              float: right;
-              margin: 5px 0px 5px 27px;
-            }
+            width: 361px;
+            float: right;
+            margin: 5px 0px 5px 27px;
           }
         `
       case 'left':
         return `
           ${theme.breakpoint.xl} {
-            figure {
-              width: 361px;
-              float: left;
-              margin: 5px 27px 5px 0px;
-            }
+            width: 361px;
+            float: left;
+            margin: 5px 27px 5px 0px;
           }
         `
     }
@@ -147,37 +133,13 @@ const InfoBoxContainer = styled.div<{ $alignment?: string }>`
   margin-bottom: 30px;
   width: fit-content;
 
-  &::after {
-    content: '';
-    display: table;
-    clear: both;
-  }
-
   ${(props) => {
     switch (props.$alignment) {
-      case 'paragraph-width': {
-        return `
-          figure {
-            margin-left: auto;
-            margin-right: auto;
-          }
-          `
-      }
-      case 'left': {
-        return `
-          figure {
-            max-width: 50%; 
-            margin-right: auto;
-          }
-          `
+      case 'center': {
+        return `margin-left: auto; margin-right: auto;`
       }
       case 'right': {
-        return `
-          figure {
-            max-width: 50%;
-            margin-left: auto;
-          }
-        `
+        return `margin-left: auto`
       }
     }
   }}
