d8c80b6b4eeb3d2567131c0efe82f83f1a5e3839
duidae <duidae@gmail.com>
2023-08-23 16:39:09 +0800

    fix: fix cta

--- packages/frontend/src/app/home/call-to-action.tsx
+++ packages/frontend/src/app/home/call-to-action.tsx
@@ -5,10 +5,29 @@ import './call-to-action.scss'
 export const CallToAction = () => {
   return (
     <div className="cta-container">
-      <a href={SUBSCRIBE_URL} className="top"></a>
+      <a href={SUBSCRIBE_URL} className="top">
+        <h3>訂閱《少年報導者》</h3>
+        <p>
+          訂閱我們，第一時間接收《少年報導者》
+          最新文章和專題，不會錯過精彩新聞。
+        </p>
+        歡迎訂閱
+      </a>
       <div className="bottom">
-        <a href={'/about#mail'} className="mail"></a>
-        <a href={'/about#post'} className="contribute"></a>
+        <a href={'/about#mail'} className="mail">
+          <h3>讀者信箱， 給我們意見</h3>
+          <p>
+            如果想給我們團隊一個鼓勵、一個建議，或是提供我們採訪線索，請寫信給我們，不要猶豫。
+          </p>
+          歡迎來信
+        </a>
+        <a href={'/about#post'} className="contribute">
+          <h3>投稿給報導仔</h3>
+          <p>
+            各位同學們，你對熱門時事或是生活、文化、社會有自己的觀察和想法嗎？歡迎你寫成文章，投稿給報導仔！
+          </p>
+          歡迎投稿
+        </a>
       </div>
     </div>
   )
