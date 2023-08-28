2ee1f1c3098bbcb9a17bfbedf01dc742b31e41b8
duidae <duidae@gmail.com>
2023-08-28 14:46:57 +0800

    fix: add category query templates

--- packages/frontend/src/app/category/[subcategory]/page.tsx
+++ packages/frontend/src/app/category/[subcategory]/page.tsx
@@ -1,7 +1,36 @@
-export default function SubCategory({
+import axios from 'axios'
+import { notFound } from 'next/navigation'
+import { API_URL } from '@/app/constants'
+
+const subcategoryQueryGQL = `
+`
+
+export default async function SubCategory({
   params,
 }: {
   params: { subcategory: string }
 }) {
+  let response
+  try {
+    response = params?.subcategory
+      ? await axios.post(API_URL, {
+          query: subcategoryQueryGQL,
+          variables: {
+            where: {
+              slug: params.subcategory,
+            },
+          },
+        })
+      : undefined
+  } catch (err) {
+    console.error('Fetch post data failed!', err)
+    notFound()
+  }
+
+  const post = response?.data?.data?.post
+  if (!post) {
+    notFound()
+  }
+
   return <h1>subcategory: {params.subcategory}</h1>
 }
