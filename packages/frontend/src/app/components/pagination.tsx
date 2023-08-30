67135828cf97f649768a6677ad905f3da69c7a4a
duidae <duidae@gmail.com>
2023-08-30 10:17:43 +0800

    fix: add pagination to list page

--- packages/frontend/src/app/components/pagination.tsx
+++ packages/frontend/src/app/components/pagination.tsx
@@ -10,14 +10,18 @@ export const Pagination = (props: PaginationProp) => {
   return (
     pageNum > 0 && (
       <div className="pagination">
-        {Array(pageNum).map((pageIndex) => {
-          return (
-            <div key={`pagination-index-${pageIndex}`}>
-              <a href={``}>{pageIndex}</a>
-            </div>
-          )
-        })}
+        {Array(pageNum)
+          .fill(0)
+          .map((pageIndex) => {
+            return (
+              <div key={`pagination-index-${pageIndex}`}>
+                <a href={``}>{pageIndex}</a>
+              </div>
+            )
+          })}
       </div>
     )
   )
 }
+
+export default Pagination
