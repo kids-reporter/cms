b4f8b97e1dc557fcb83a1139c48c094cc65004d1
duidae <duidae@gmail.com>
2023-08-30 10:08:31 +0800

    fix: add key

---
+++ packages/frontend/src/app/components/pagination.tsx
@@ -0,0 +1,19 @@
+import './pagination.scss'
+
+type PaginationProp = {
+  pageNum: number
+}
+
+export const Pagination = (props: PaginationProp) => {
+  const pageNum = props?.pageNum
+
+  return (
+    pageNum > 0 && (
+      <div className="pagination">
+        {Array(pageNum).map((pageIndex) => {
+          return <div key={`pagination-index-${pageIndex}`}>{pageIndex}</div>
+        })}
+      </div>
+    )
+  )
+}
