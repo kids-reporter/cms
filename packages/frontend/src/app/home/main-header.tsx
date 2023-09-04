f164f0b530b592b63624af1294936c34c34a93e7
duidae <duidae@gmail.com>
2023-09-04 16:25:30 +0800

    fix: fix main header style

--- packages/frontend/src/app/home/main-header.tsx
+++ packages/frontend/src/app/home/main-header.tsx
@@ -30,6 +30,34 @@ export const MainHeader = () => {
     </nav>
   )
 
+  const searchInput = (
+    <form
+      role="search"
+      method="get"
+      action="https://kids.twreporter.org/"
+      aria-haspopup="listbox"
+    >
+      <input
+        type="search"
+        placeholder="搜尋更多新聞、議題"
+        value=""
+        name="s"
+        title="Search for..."
+        aria-label="Search for..."
+        onChange={onHandleSearch}
+      />
+      <button type="submit" className="search-submit" aria-label="搜尋按鈕">
+        {SearchIcon}
+        <span data-loader="circles">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </button>
+      <input type="hidden" name="post_type" value="post" />
+    </form>
+  )
+
   return (
     <div className="main-header">
       <div className="left">
@@ -45,41 +73,13 @@ export const MainHeader = () => {
         {navi}
       </div>
       <div className="right">
-        <div>
-          <form
-            role="search"
-            method="get"
-            action="https://kids.twreporter.org/"
-            aria-haspopup="listbox"
-          >
-            <input
-              type="search"
-              placeholder="搜尋更多新聞、議題"
-              value=""
-              name="s"
-              title="Search for..."
-              aria-label="Search for..."
-              onChange={onHandleSearch}
-            />
-            <button
-              type="submit"
-              className="search-submit"
-              aria-label="搜尋按鈕"
-            >
-              {SearchIcon}
-              <span data-loader="circles">
-                <span></span>
-                <span></span>
-                <span></span>
-              </span>
-            </button>
-            <input type="hidden" name="post_type" value="post" />
-          </form>
-        </div>
-        <div className="btn-group">
-          {ContributeBtn}
-          {SubscribeBtn}
-          {AboutUsBtn}
+        <div className="content">
+          {searchInput}
+          <div className="btn-group">
+            {ContributeBtn}
+            {SubscribeBtn}
+            {AboutUsBtn}
+          </div>
         </div>
       </div>
     </div>
