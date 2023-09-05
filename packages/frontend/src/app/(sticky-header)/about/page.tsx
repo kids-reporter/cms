4d5e58d143834dc68d044d9e0394a22bb4a0dacf
duidae <duidae@gmail.com>
2023-09-05 11:17:13 +0800

    fix: add about

--- packages/frontend/src/app/(sticky-header)/about/page.tsx
+++ packages/frontend/src/app/(sticky-header)/about/page.tsx
@@ -5,5 +5,28 @@ import './page.scss'
 import { authorsMockup } from '@/app/mockup'
 
 export default function About() {
-  return <AuthorCard title="誰在為你服務" authors={authorsMockup} />
+  return (
+    <main>
+      <div className="who-we-are">
+        <img src={'/images/about_who_we_are.svg'} />
+      </div>
+      <div className="tell-you"></div>
+      <div className="news">
+        <span>在這裡，你們可以這樣看新聞：</span>
+        <div className="pics">
+          <img src={'/images/about_news_pic1.svg'} />
+          <img src={'/images/about_news_pic2.svg'} />
+          <img src={'/images/about_news_pic3.svg'} />
+          <img src={'/images/about_news_pic4.svg'} />
+          <img src={'/images/about_news_pic5.svg'} />
+        </div>
+      </div>
+      <div className="subscribe"></div>
+      <div className="contribute"></div>
+      <div className="mail"></div>
+      <AuthorCard title="誰在為你服務" authors={authorsMockup} />
+      <AuthorCard title="我們的顧問" authors={authorsMockup} />
+      <div className="donate"></div>
+    </main>
+  )
 }
