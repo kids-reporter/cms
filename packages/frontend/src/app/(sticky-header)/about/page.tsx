8b976aad70da55dc7a8b327b0a49e4d58c084535
duidae <duidae@gmail.com>
2023-09-07 11:36:50 +0800

    fix: fix about style

--- packages/frontend/src/app/(sticky-header)/about/page.tsx
+++ packages/frontend/src/app/(sticky-header)/about/page.tsx
@@ -81,13 +81,28 @@ export default function About() {
         《少年報導者》是一個開放的公共平台，報導仔希望聽見大家的看法和心聲，歡迎10～15歲的同學投稿給報導仔，針對新聞時事、國家政策、校園生活，或是藝術文化、運動體育，都可以寫下你的觀點、評論，讓報導仔協助你成為我們的評論員。
       </p>
       <img className="road" src={'/images/about_road.svg'} />
-      <p>
-        投稿都會刊登嗎？ •編輯群和專家會做討論 •如果刊登你會收到通知
-        •不合適刊登的文章不會另行通知
-      </p>
-      <p>
-        刊登有什麼獎勵？ •你會收到微薄稿酬 •你會收到「少年報導者評論員」證書
-      </p>
+      <div className="desc">
+        <span>
+          投稿都會刊登嗎？
+          <p>
+            <br />
+            •編輯群和專家會做討論
+            <br />
+            •如果刊登你會收到通知
+            <br />
+            •不合適刊登的文章不會另行通知
+          </p>
+        </span>
+        <span>
+          刊登有什麼獎勵？
+          <p>
+            <br />
+            •你會收到微薄稿酬
+            <br />
+            •你會收到「少年報導者評論員」證書
+          </p>
+        </span>
+      </div>
       <img
         className="certification"
         src={'/images/about_certification_template.jpg'}
