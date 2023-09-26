45f98b0c887f1ebadda35be8a1f25b78498a5b31
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-09-27 01:09:50 +0800

    feat(frontend): add search page

---
+++ packages/frontend/src/app/(sticky-header)/search/cards.tsx
@@ -0,0 +1,57 @@
+'use client'
+import PostCard, { PostCardProp } from '@/app/components/post-card'
+import styled from 'styled-components'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { createRef } from 'react'
+import { mediaQuery } from '@/app/utils/media-query'
+
+const FlexContainer = styled(TransitionGroup)`
+  /* flexbox related styles */
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  row-gap: 30px;
+  column-gap: 30px;
+  flex-wrap: wrap;
+  margin: 0 auto 30px auto;
+
+  ${mediaQuery.mediumAbove} {
+    max-width: 1440px;
+  }
+
+  .item-enter {
+    opacity: 0;
+  }
+  .item-enter-done {
+    opacity: 1;
+    transition: opacity 500ms ease-in;
+  }
+`
+
+export type { PostCardProp }
+
+const Cards = ({ items }: { items: PostCardProp[] }) => {
+  const cardsJsx = items.map((item, idx) => {
+    const nodeRef = createRef<HTMLDivElement>()
+    if (!item) {
+      return null
+    }
+    return (
+      <CSSTransition
+        in={true}
+        key={idx}
+        nodeRef={nodeRef}
+        timeout={500}
+        classNames="item"
+      >
+        <div ref={nodeRef}>
+          <PostCard post={item.post} showDesc={item?.showDesc} />
+        </div>
+      </CSSTransition>
+    )
+  })
+  return <FlexContainer>{cardsJsx}</FlexContainer>
+}
+
+export { Cards }
+export default Cards
---
+++ packages/frontend/src/app/(sticky-header)/search/page.tsx
@@ -0,0 +1,66 @@
+import errors from '@twreporter/errors'
+import { LoadMoreResults } from './results'
+import { SearchTitle } from './styled'
+import {
+  getPostOnlySearchResults,
+  transferItemsToPostCards,
+  defaultCount,
+} from '@/app/api/search/route'
+
+const apiKey = process.env.SEARCH_API_KEY || ''
+const cx = process.env.SEARCH_ENGINE_ID || ''
+
+export default async function SearchPage({
+  searchParams,
+}: {
+  searchParams: {
+    q: string
+  }
+}) {
+  if (!searchParams.q) {
+    return <SearchTitle>請輸入要搜尋的字串。</SearchTitle>
+  }
+
+  let data
+  try {
+    data = await getPostOnlySearchResults({
+      q: searchParams.q,
+      apiKey,
+      cx,
+      start: 1,
+      count: defaultCount,
+    })
+  } catch (err) {
+    console.log(
+      JSON.stringify({
+        severity: 'ERROR',
+        message: errors.helpers.printAll(
+          err,
+          { withStack: true, withPayload: true },
+          0,
+          0
+        ),
+      })
+    )
+    return (
+      <SearchTitle>
+        搜尋結果服務異常，請稍候再試。 若持續發生，煩請來信至
+        kidsnews@twreporter.org。
+      </SearchTitle>
+    )
+  }
+
+  const cardItems = Array.isArray(data?.items)
+    ? transferItemsToPostCards(data.items)
+    : []
+
+  return (
+    <div>
+      <SearchTitle>搜尋結果 {searchParams.q}</SearchTitle>
+      <LoadMoreResults
+        currentCardItems={cardItems}
+        nextQuery={data.nextQuery}
+      />
+    </div>
+  )
+}
