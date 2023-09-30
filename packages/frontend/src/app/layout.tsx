2af2c24f1739110a4c84f76f45233fa77d3eb6c2
duidae <duidae@gmail.com>
2023-10-01 00:50:07 +0800

    fix : add gtm script

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -1,8 +1,12 @@
 import { Metadata } from 'next'
+// import Script from 'next/script'
 import BackToTop from '@/app/components/back-to-top'
 import Footer from '@/app/components/footer'
 import StyledComponentsRegistry from './registry'
 import './globals.scss'
+
+// TODO: enable GTM
+// const gtmID = 'GTM-T37WZJ44'
 
 export const metadata: Metadata = {
   title: '少年報導者 The Reporter for Kids - 理解世界 參與未來',
@@ -17,11 +21,29 @@ export default function RootLayout({
 }) {
   return (
     <html>
+      {/*
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${gtmID}');
+        `}
+      </Script>
+      */}
       <StyledComponentsRegistry>
         <body>
           {children}
           <BackToTop />
           <Footer />
+          {/*
+          <noscript
+            dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+            }}
+          />
+          */}
         </body>
       </StyledComponentsRegistry>
     </html>
