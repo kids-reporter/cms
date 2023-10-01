0f4ec15d232bfbd5c9137934406e4ed1b5929e2b
duidae <duidae@gmail.com>
2023-10-01 21:05:18 +0800

    fix: fix category nagivator

--- packages/frontend/src/app/(sticky-header)/category/[subcategory]/page.tsx
+++ packages/frontend/src/app/(sticky-header)/category/[subcategory]/page.tsx
@@ -7,32 +7,45 @@ import { API_URL } from '@/app/constants'
 import './page.scss'
 
 // TODO: remove mockup
-import { MOCKUP_TAGS, postMockupsMore } from '@/app/mockup'
+import { postMockupsMore } from '@/app/mockup'
 
-const categoryGQL = `
+const subcategoriesGQL = `
+query($where: CategoryWhereUniqueInput!) {
+  category(where: $where) {
+    subcategories {
+      name
+      slug
+    }
+  }
+}
 `
 
-const postQueryGQL = `
-`
+// const postQueryGQL = ``
 
 export default async function SubCategory({
   params,
 }: {
   params: { subcategory: string }
 }) {
-  let categoryRes, postRes
-  try {
-    categoryRes = params?.subcategory
-      ? await axios.post(API_URL, {
-          query: categoryGQL,
-          variables: {
-            where: {
-              slug: params.subcategory,
-            },
-          },
-        })
-      : undefined
+  const category = params?.subcategory
 
+  if (!category) {
+    console.error('Incorrect category!', category)
+    notFound()
+  }
+
+  let subcategoriesRes
+  try {
+    subcategoriesRes = await axios.post(API_URL, {
+      query: subcategoriesGQL,
+      variables: {
+        where: {
+          slug: category,
+        },
+      },
+    })
+
+    /*
     postRes = params?.subcategory
       ? await axios.post(API_URL, {
           query: postQueryGQL,
@@ -43,27 +56,29 @@ export default async function SubCategory({
           },
         })
       : undefined
+    */
   } catch (err) {
-    console.error('Fetch post data failed!', err)
+    console.error('Fetch category data failed!', err)
     notFound()
   }
 
-  const categoryTags = categoryRes?.data?.map((category: any) => {
-    return {
-      name: category.name,
-      slug: category.slug,
-    }
-  })
-  const post = postRes?.data?.data?.post
-  if (!categoryTags || !post) {
-    notFound()
-  }
+  const subCategories = [{ name: '所有文章', slug: category }]
+  subCategories.push(
+    ...subcategoriesRes?.data?.data?.category?.subcategories?.map(
+      (subcategory: any) => {
+        return {
+          name: subcategory.name,
+          slug: `${category}/${subcategory.slug}`,
+        }
+      }
+    )
+  )
 
   return (
     <main className="container">
       <div className="content">
         <img src={'/images/category_news.svg'} />
-        <Tags tags={MOCKUP_TAGS} />
+        <Tags tags={subCategories} />
         <div className="post-list">
           {postMockupsMore.map((post, index) => {
             return <PostCard key={`author-post-card-${index}`} post={post} />
