c90d566168bdba613e0654949552b68044dbc819
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-10-02 15:54:27 +0800

    feat(cms): add `relatedPostsCount` field in category and subcategory list

--- packages/cms/lists/subcatgory.ts
+++ packages/cms/lists/subcatgory.ts
@@ -98,6 +98,58 @@ const listConfigurations = list({
         },
       },
     }),
+    relatedPostsCount: virtual({
+      field: () =>
+        graphql.field({
+          type: graphql.Int,
+          async resolve(item: Record<string, any>, args, context) {
+            // subcategory id
+            const itemId = item?.id
+
+            // find subcategory item via GQL query
+            const category = await context.query.Subcategory.findOne({
+              where: { id: itemId },
+              query: 'subSubcategories { id slug }',
+            })
+
+            // collect all subSubcategory ids under a category
+            const subSubcategoryIds = collectSubSubcategoryIds(
+              category as SubcategoryItem
+            )
+
+            if (subSubcategoryIds.length === 0) {
+              return 0
+            }
+
+            // find published posts with
+            // at least one specified subSubcategory
+            const count = await context.prisma.Post.count({
+              where: {
+                OR: [{ status: 'published' }, { status: 'archived' }],
+                subSubcategories: {
+                  some: {
+                    id: {
+                      in: subSubcategoryIds,
+                    },
+                  },
+                },
+              },
+            })
+            return count
+          },
+        }),
+      ui: {
+        createView: {
+          fieldMode: 'hidden',
+        },
+        itemView: {
+          fieldMode: 'hidden',
+        },
+        listView: {
+          fieldMode: 'hidden',
+        },
+      },
+    }),
     createdAt: timestamp({
       defaultValue: { kind: 'now' },
     }),
