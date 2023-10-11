d3aa2dd3a4ef6ad0f5f1fc5dab32264d0bec4c5e
duidae <duidae@gmail.com>
2023-10-11 16:14:26 +0800

    fix: add skeleton to post renderer

--- packages/frontend/src/app/(sticky-header)/article/[slug]/post-renderer.tsx
+++ packages/frontend/src/app/(sticky-header)/article/[slug]/post-renderer.tsx
@@ -1,13 +1,26 @@
 'use client'
 import { useEffect, useState } from 'react'
 import { useArticleContext } from './article-context'
+import styled from 'styled-components'
+import Skeleton from 'react-loading-skeleton'
 import { ArticleBodyDraftRenderer } from '@kids-reporter/draft-renderer'
 import { Theme } from '@/app/constants'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 type PostProp = {
   post: any
   theme: Theme
 }
+
+const SkeletonContainer = styled.div`
+  width: 80%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 40px;
+`
 
 export const PostRenderer = (props: PostProp) => {
   const [isMounted, setIsMounted] = useState(false)
@@ -15,19 +28,22 @@ export const PostRenderer = (props: PostProp) => {
     setIsMounted(true)
   }, [])
 
+  const content = props.post?.content
+  const theme = props.theme
   const { fontSize } = useArticleContext()
 
-  // TODO: render skeleton
-  return (
-    isMounted &&
-    props?.post?.content &&
-    props?.theme && (
+  return isMounted ? (
+    content && theme && (
       <ArticleBodyDraftRenderer
         rawContentState={props.post.content}
         fontSizeLevel={fontSize}
         themeColor={props.theme}
       />
     )
+  ) : (
+    <SkeletonContainer>
+      <Skeleton count={10} />
+    </SkeletonContainer>
   )
 }
 
