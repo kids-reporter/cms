711eb0124d865bb64be452d8957f449d6d894c6b
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-10-12 11:44:50 +0800

    fix(frontend): add `NEXT_PUBLIC_` prefix for client side usage

--- packages/frontend/src/app/environment-variables.ts
+++ packages/frontend/src/app/environment-variables.ts
@@ -1,8 +1,9 @@
-const { CMS_ORIGIN, GQL_ENDPOINT } = process.env
+const { NEXT_PUBLIC_CMS_ORIGIN, NEXT_PUBLIC_GQL_ENDPOINT } = process.env
 
-export const cmsOrigin = CMS_ORIGIN || 'https://dev-kids-cms.twreporter.org'
+export const cmsOrigin =
+  NEXT_PUBLIC_CMS_ORIGIN || 'https://dev-kids-cms.twreporter.org'
 export const gqlEndpoint =
-  GQL_ENDPOINT || 'https://dev-kids-cms.twreporter.org/api/graphql'
+  NEXT_PUBLIC_GQL_ENDPOINT || 'https://dev-kids-cms.twreporter.org/api/graphql'
 
 export default {
   cmsOrigin,
