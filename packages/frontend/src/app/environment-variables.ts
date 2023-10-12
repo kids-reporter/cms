00e48e1f6d0d2205342ab61f2ac491d39260d4ff
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-10-12 16:05:56 +0800

    revert: "refactor(frontend): use `NEXT_PUBLIC_RELEASE_TARGET` env variable to decide `API_URL` and `CMS_URL`"
    This reverts commit 34499b18cdd29e0a108fec4ee7998e3b146fbbb9.

--- packages/frontend/src/app/environment-variables.ts
+++ packages/frontend/src/app/environment-variables.ts
@@ -1,5 +1,10 @@
-export const releaseTarget = process.env.NEXT_PUBLIC_RELEASE_TARGET || 'local'
+export const cmsOrigin =
+  process.env.NEXT_PUBLIC_CMS_ORIGIN || 'https://dev-kids-cms.twreporter.org'
+export const gqlEndpoint =
+  process.env.NEXT_PUBLIC_GQL_ENDPOINT ||
+  'https://dev-kids-cms.twreporter.org/api/graphql'
 
 export default {
-  releaseTarget,
+  cmsOrigin,
+  gqlEndpoint,
 }
