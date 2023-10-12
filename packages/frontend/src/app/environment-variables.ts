6107bf5e59dcef519969b31bde8ac55c4459ff52
nickhsine <nick@nickhsinedeMacBook-Pro.local>
2023-10-12 12:26:32 +0800

    fix(frontend): destruct variables from `process.env` not working

--- packages/frontend/src/app/environment-variables.ts
+++ packages/frontend/src/app/environment-variables.ts
@@ -1,9 +1,8 @@
-const { NEXT_PUBLIC_CMS_ORIGIN, NEXT_PUBLIC_GQL_ENDPOINT } = process.env
-
 export const cmsOrigin =
-  NEXT_PUBLIC_CMS_ORIGIN || 'https://dev-kids-cms.twreporter.org'
+  process.env.NEXT_PUBLIC_CMS_ORIGIN || 'https://dev-kids-cms.twreporter.org'
 export const gqlEndpoint =
-  NEXT_PUBLIC_GQL_ENDPOINT || 'https://dev-kids-cms.twreporter.org/api/graphql'
+  process.env.NEXT_PUBLIC_GQL_ENDPOINT ||
+  'https://dev-kids-cms.twreporter.org/api/graphql'
 
 export default {
   cmsOrigin,
