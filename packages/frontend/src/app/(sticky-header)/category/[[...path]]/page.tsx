643fbd74858929e81a9d96c27664a4a254a0429c
duidae <duidae@gmail.com>
2023-10-17 00:07:43 +0800

    fix: add subsubcategory query

--- packages/frontend/src/app/(sticky-header)/category/[[...path]]/page.tsx
+++ packages/frontend/src/app/(sticky-header)/category/[[...path]]/page.tsx
@@ -46,6 +46,36 @@ query($where: SubcategoryWhereUniqueInput!, $take: Int!, $skip: Int!) {
 }
 `
 
+const subSubcategoryPostsGQL = `
+query($where: SubSubcategoryWhereUniqueInput!, $take: Int!, $skip: Int!) {
+  subSubcategory(where: $where) {
+    relatedPosts(take: $take, skip: $skip) {
+      ${POST_CONTENT_GQL}
+    }
+    relatedPostsCount
+  }
+}
+`
+
+const getImageAndThemeFromCategory = (category: string) => {
+  let imageURL, theme
+  if (category === 'news') {
+    imageURL = '/images/category_news.svg'
+    theme = Theme.BLUE
+  } else if (category === 'listening-news') {
+    imageURL = '/images/category_listening_news.svg'
+    theme = Theme.RED
+  } else if (category === 'comics') {
+    imageURL = '/images/category_comics.svg'
+    theme = Theme.YELLOW
+  } else {
+    imageURL = '/images/category_campus.svg'
+    theme = Theme.YELLOW
+  }
+  return { imageURL, theme }
+}
+
+// TODO: fix bug of /category/news/knowledge/page
 export default async function Category({ params }: { params: { path: any } }) {
   const path = params.path
   if (!path || !Array.isArray(path) || path.length === 0) {
@@ -53,22 +83,35 @@ export default async function Category({ params }: { params: { path: any } }) {
     notFound()
   }
 
+  // Category page routing scenarios:   ex: /category/path[0]/path[1]/path[2]...
+  // length = 1(category)               ex: /category/news
+  // length = 2(subcategory)            ex: /category/news/times
+  // length = 3(subSubcategory)         ex: /category/news/times/medical-news
+  // length = 3(category, page 2)       ex: /category/news/page/2
+  // length = 4(subcategory, page 2)    ex: /category/news/times/page/2
+  // length = 5(subSubcategory, page 2) ex: /category/news/times/medical-news/page/2
   let category = '',
     subcategory = '',
+    subSubcategory = '',
     currentPage = 1
   if (path.length === 1 && path[0]) {
     category = path[0]
   } else if (path.length === 2 && path[1] && path[1] !== 'page') {
     category = path[0]
     subcategory = path[1]
-  } else if (
-    path.length === 3 &&
-    path[1] === 'page' &&
-    Number.isInteger(Number(path[2])) &&
-    Number(path[2]) > 0
-  ) {
-    category = path[0]
-    currentPage = Number(path[2])
+  } else if (path.length === 3) {
+    if (
+      path[1] === 'page' &&
+      Number.isInteger(Number(path[2])) &&
+      Number(path[2]) > 0
+    ) {
+      category = path[0]
+      currentPage = Number(path[2])
+    } else {
+      category = path[0]
+      subcategory = path[1]
+      subSubcategory = path[2]
+    }
   } else if (
     path.length === 4 &&
     path[1] !== 'page' &&
@@ -79,6 +122,16 @@ export default async function Category({ params }: { params: { path: any } }) {
     category = path[0]
     subcategory = path[1]
     currentPage = Number(path[3])
+  } else if (
+    path.length === 5 &&
+    path[3] === 'page' &&
+    Number.isInteger(Number(path[4])) &&
+    Number(path[4]) > 0
+  ) {
+    category = path[0]
+    subcategory = path[1]
+    subSubcategory = path[2]
+    currentPage = Number(path[4])
   } else {
     console.error('Incorrect category path!', path)
     notFound()
@@ -118,11 +171,21 @@ export default async function Category({ params }: { params: { path: any } }) {
     notFound()
   }
 
-  // Fetch related posts of category/subcategory
+  // Fetch related posts of subSubcategory/subcategory/category
   let posts: PostSummary[], postsCount
   try {
-    const query = subcategory ? subcategoryPostsGQL : categoryPostsGQL
-    const slug = subcategory ? subcategory : category
+    let query, slug
+    if (subSubcategory) {
+      query = subSubcategoryPostsGQL
+      slug = subSubcategory
+    } else if (subcategory) {
+      query = subcategoryPostsGQL
+      slug = subcategory
+    } else {
+      query = categoryPostsGQL
+      slug = category
+    }
+
     const postsRes = await axios.post(API_URL, {
       query: query,
       variables: {
@@ -133,35 +196,40 @@ export default async function Category({ params }: { params: { path: any } }) {
         skip: (currentPage - 1) * POST_PER_PAGE,
       },
     })
-    posts = GetPostSummaries(
-      subcategory
-        ? postsRes?.data?.data?.subcategory?.relatedPosts
-        : postsRes?.data?.data?.category?.relatedPosts
-    )
-    postsCount = subcategory
-      ? postsRes?.data?.data?.subcategory?.relatedPostsCount
-      : postsRes?.data?.data?.category?.relatedPostsCount
+
+    if (subSubcategory) {
+      posts = GetPostSummaries(
+        postsRes?.data?.data?.subSubcategory?.relatedPosts
+      )
+      postsCount = postsRes?.data?.data?.subSubcategory?.relatedPostsCount
+    } else if (subcategory) {
+      posts = GetPostSummaries(postsRes?.data?.data?.subcategory?.relatedPosts)
+      postsCount = postsRes?.data?.data?.subcategory?.relatedPostsCount
+    } else {
+      posts = GetPostSummaries(postsRes?.data?.data?.category?.relatedPosts)
+      postsCount = postsRes?.data?.data?.category?.relatedPostsCount
+    }
   } catch (err) {
     console.error('Fetch posts failed!', err)
     notFound()
   }
 
   const totalPages = Math.ceil(postsCount / POST_PER_PAGE)
-
-  let imageURL, theme
-  if (category === 'news') {
-    imageURL = '/images/category_news.svg'
-    theme = Theme.BLUE
-  } else if (category === 'listening-news') {
-    imageURL = '/images/category_listening_news.svg'
-    theme = Theme.RED
-  } else if (category === 'comics') {
-    imageURL = '/images/category_comics.svg'
-    theme = Theme.YELLOW
-  } else {
-    imageURL = '/images/category_campus.svg'
-    theme = Theme.YELLOW
+  if (currentPage > totalPages) {
+    console.error('Incorrect page!', currentPage)
+    notFound()
   }
+
+  let routingPrefix
+  if (subSubcategory) {
+    routingPrefix = `/category/${category}/${subcategory}/${subSubcategory}/page`
+  } else if (subcategory) {
+    routingPrefix = `/category/${category}/${subcategory}/page`
+  } else {
+    routingPrefix = `/category/${category}/page`
+  }
+
+  const { imageURL, theme } = getImageAndThemeFromCategory(category)
 
   return (
     <main className="container">
@@ -200,11 +268,7 @@ export default async function Category({ params }: { params: { path: any } }) {
           <Pagination
             currentPage={currentPage}
             totalPages={totalPages}
-            routingPrefix={
-              subcategory
-                ? `/category/${category}/${subcategory}/page`
-                : `/category/${category}/page`
-            }
+            routingPrefix={routingPrefix}
           />
         )}
       </div>
