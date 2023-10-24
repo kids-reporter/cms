5ff7875b63038ab0b2af022486913ebb5f4743d7
duidae <duidae@gmail.com>
2023-10-24 15:45:51 +0800

    fix: remove log

--- packages/frontend/src/middleware.ts
+++ packages/frontend/src/middleware.ts
@@ -6,7 +6,6 @@ export const config = {
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
-  console.log(`${process.env.IMAGE_ORIGIN}${pathname}`)
   return NextResponse.redirect(
     new URL(`${process.env.IMAGE_ORIGIN}${pathname}`, request.url)
   )
