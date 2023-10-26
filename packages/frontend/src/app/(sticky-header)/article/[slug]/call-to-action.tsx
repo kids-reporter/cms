872fe17bfbc80726756ded098030305143e313e5
duidae <duidae@gmail.com>
2023-10-26 22:54:45 +0800

    fix: fix cta in article

--- packages/frontend/src/app/(sticky-header)/article/[slug]/call-to-action.tsx
+++ packages/frontend/src/app/(sticky-header)/article/[slug]/call-to-action.tsx
@@ -17,29 +17,18 @@ export const CallToAction = () => {
                 一篇豐富、精彩和專業的報導，要經過記者、攝影、設計師、編輯，還有許多專家才能完成，完成後還要靠著社群編輯、行銷企劃，才能送到你的眼前。我們所有的努力，都希望能幫助你更了解這個世界，更希望你對這個世界發出提問。讓每一篇報導點亮世界，訂閱我們、歡迎投稿。
               </p>
             </div>
-            <div
-              className="wp-block-stackable-button-group stk-block-button-group stk-block stk-b130b7f"
-              data-block-id="b130b7f"
-              style={{
-                maxWidth: '300px !important',
-                minWidth: 'auto !important',
-                marginRight: 'auto !important',
-                marginLeft: '0px !important',
-              }}
-            >
-              <div className="action-container">
-                <a
-                  className="subscribe-btn"
-                  href={SUBSCRIBE_URL}
-                  target="_blank"
-                  rel="noreferrer noopener"
-                >
-                  <span>歡迎訂閱</span>
-                </a>
-                <a className="contribute-btn" href={contributeURL}>
-                  <span>歡迎投稿</span>
-                </a>
-              </div>
+            <div className="action-container">
+              <a
+                className="subscribe-btn"
+                href={SUBSCRIBE_URL}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <span>歡迎訂閱</span>
+              </a>
+              <a className="contribute-btn" href={contributeURL}>
+                <span>歡迎投稿</span>
+              </a>
             </div>
           </div>
         </div>
