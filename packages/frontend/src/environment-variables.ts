fe4ff40d51f6103dbaca673098680c72f79c4807
duidae <duidae@gmail.com>
2023-11-15 10:53:10 +0800

    fix(fe): replace cms with storage

--- packages/frontend/src/environment-variables.ts
+++ packages/frontend/src/environment-variables.ts
@@ -1,5 +1,5 @@
 export const storageOrigin =
-  process.env.NEXT_PUBLIC_CMS_ORIGIN || 'http://localhost:3001'
+  process.env.NEXT_PUBLIC_STORAGE_ORIGIN || 'http://localhost:3001'
 export const gqlEndpoint =
   process.env.NEXT_PUBLIC_GQL_ENDPOINT || 'http://localhost:3001/api/graphql'
 export const isPreviewMode = process.env.NEXT_PUBLIC_IS_PREVIEW_MODE === 'true'
