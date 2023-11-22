fa32a79a6cc0f0ed60a7e5b517be748f1b7c17f7
duidae <duidae@gmail.com>
2023-11-22 15:38:57 +0800

    fix(fe): add robots.txt

---
+++ packages/frontend/src/app/robots.ts
@@ -0,0 +1,21 @@
+import { MetadataRoute } from 'next'
+import { KIDS_URL_ORIGIN } from '@/app/constants'
+
+const devConfig = {
+  rules: {
+    userAgent: '*',
+    disallow: '/',
+  },
+}
+
+const prodConfig = {
+  rules: {
+    userAgent: '*',
+    allow: '/',
+  },
+  sitemap: `${KIDS_URL_ORIGIN}/sitemaps/all.xml`,
+}
+
+export default function robots(): MetadataRoute.Robots {
+  return process.env.NEXT_PUBLIC_IS_PRODUCTION ? prodConfig : devConfig
+}
