24e71755eae0f8263b9961b78f3a8f5f96e68421
Howar31 <howar31@gmail.com>
2023-11-26 22:48:54 +0800

    feat(cms): add preview link in post list

---
+++ packages/cms/lists/views/cell-button.tsx
@@ -0,0 +1,14 @@
+import { CellLink } from '@keystone-6/core/admin-ui/components'
+import { CellComponent } from '@keystone-6/core/types'
+import React from 'react'
+
+export const Cell: CellComponent = ({ item }) => {
+  console.log(item)
+  const value = item.listPreview
+  return (
+    <CellLink href={value} target="_blank">
+      Preview
+    </CellLink>
+  )
+}
+Cell.supportsLinkTo = true
