a497e554887c99cd13a19f2171b8a2eaaa7d3210
Howar31 <howar31@gmail.com>
2023-11-30 11:58:56 +0800

    fix(imageResizer): fix reviews

--- packages/image-resizer/index.js
+++ packages/image-resizer/index.js
@@ -1,12 +1,11 @@
 import { config } from './configs.js'
-import { log } from './utils.js'
+// import { log } from './utils.js'
 import { Storage } from '@google-cloud/storage'
 import sharp from 'sharp'
 import { join, basename, extname } from 'path'
 import { tmpdir } from 'os'
 import { promises as fs } from 'fs'
 import express from 'express'
-import bodyParser from 'body-parser'
 
 const storage =
   config.gcs.projectId && config.gcs.keyFilename
@@ -18,28 +17,30 @@ const storage =
 
 const app = express()
 
-app.use(bodyParser.json())
+app.use(express.json())
 app.post('/', async (req, res) => {
   const event = req.body
 
   // Extract bucket and file name from protoPayload.resourceName
-  const resourceName = event.protoPayload.resourceName
+  const resourceName = event?.protoPayload?.resourceName
+  if (!resourceName || typeof resourceName !== 'string') {
+    res.status(400).send('Invalid resourceName')
+    return
+  }
   const bucket = resourceName.split('/')[3]
   const name = resourceName.split('/').slice(5).join('/')
 
-  log(`Processing file ${name} in bucket ${bucket}`)
+  console.log(`Processing file ${name} in bucket ${bucket}`)
 
   const file = storage.bucket(bucket).file(name)
 
   // Check if the file exists in GCS
   const [exists] = await file.exists()
   if (!exists) {
-    log(`File ${name} does not exist in bucket ${bucket}`)
+    console.error(`File ${name} does not exist in bucket ${bucket}`)
     res.status(404).send(`File ${name} does not exist`)
     return
   }
-  const tempFilePath = join(tmpdir(), 'tempImage')
-
   // Skip unsupported file types, sharp only accepts jpeg, png, gif, webp
   const [metadata] = await file.getMetadata()
   const contentType = metadata.contentType || 'undefined'
@@ -48,12 +49,12 @@ app.post('/', async (req, res) => {
       contentType
     )
   ) {
-    log(`Skipping unsupported file: ${name} (${contentType})`)
-    await fs.unlink(tempFilePath)
+    console.error(`Skipping unsupported file: ${name} (${contentType})`)
     res.status(400).send('Unsupported file type')
     return
   }
 
+  const tempFilePath = join(tmpdir(), 'tempImage')
   await file.download({ destination: tempFilePath })
 
   // Resize the image and upload to the target folder
@@ -78,7 +79,7 @@ app.post('/', async (req, res) => {
 
   await fs.unlink(tempFilePath)
 
-  log(`Resized ${name} to ${sizes.join(', ')}`)
+  console.log(`Resized ${name} to ${sizes.join(', ')}`)
   res.status(200).send(`Resized ${name} to ${sizes.join(', ')}`)
 })
 
