009859edd8dc1e0b03a4f5060a2370a00f78cec7
Howar31 <howar31@gmail.com>
2023-12-04 19:43:24 +0800

    fix(rss): structured log/error

--- packages/cronjob-rss-feed/utils.js
+++ packages/cronjob-rss-feed/utils.js
@@ -1,10 +1,17 @@
 import { config } from './configs.js'
 import { IncomingWebhook } from '@slack/webhook'
+import { Logging } from '@google-cloud/logging'
+// @ts-ignore `@twreporter/errors` does not have tyepscript definition file yet
+import _errors from '@twreporter/errors'
 
-const sendSlackNotification = async (message, type = 'log') => {
+// @twreporter/errors is a cjs module, therefore, we need to use its default property
+const errors = _errors.default
+const logging = new Logging()
+
+const sendSlackNotification = async (message, type = 'info') => {
   try {
     const webhook = new IncomingWebhook(
-      type === 'error' ? config.slackErrorHook : config.slackLogHook
+      type === 'error' ? config.slackErrorHook || '' : config.slackLogHook || ''
     )
     await webhook.send({
       blocks: [
@@ -54,23 +61,44 @@ const sendSlackNotification = async (message, type = 'log') => {
     })
     console.log('Slack notification sent')
   } catch (error) {
-    await log('Error sending Slack notification', error)
+    console.error('Error sending Slack notification', error)
   }
 }
 
 export const log = async (message, error = '', type = '') => {
   if (type === '') {
-    type = error === '' ? 'log' : 'error'
+    type = error === '' ? 'info' : 'error'
   }
+
+  const logName = 'cronjob-rss-feed'
+  const log = logging.log(logName)
+  const metadata = {
+    resource: { type: 'global' },
+  }
+
+  const severity = type.toUpperCase()
+
+  let data
   if (type === 'error') {
-    console.error(message, error)
-    if (config.slackErrorHook) {
-      await sendSlackNotification(`${message}: ${error.toString()}`, type)
+    data = {
+      message: errors.helpers.printAll(error, {
+        withStack: true,
+        withPayload: true,
+      }),
+      severity: severity,
     }
-  } else if (type === 'log') {
-    console.log(message, error)
+    console.error(message, error)
+  } else {
+    data = {
+      message: message,
+      severity: severity,
+    }
     if (config.slackLogHook) {
       await sendSlackNotification(message, type)
     }
+    console.log(message)
   }
+
+  const entry = log.entry(metadata, data)
+  await log.write(entry)
 }
