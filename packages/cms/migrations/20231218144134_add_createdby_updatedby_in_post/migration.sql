f0c78db02aaba553cea6e4f6e6cc1cfc9547f37d
Howar31 <howar31@gmail.com>
2023-12-20 12:49:26 +0800

    fix(cms): add migration for default user

--- packages/cms/migrations/20231218144134_add_createdby_updatedby_in_post/migration.sql
+++ packages/cms/migrations/20231218144134_add_createdby_updatedby_in_post/migration.sql
@@ -13,3 +13,9 @@ ALTER TABLE "Post" ADD CONSTRAINT "Post_createdBy_fkey" FOREIGN KEY ("createdBy"
 
 -- AddForeignKey
 ALTER TABLE "Post" ADD CONSTRAINT "Post_updatedBy_fkey" FOREIGN KEY ("updatedBy") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- Create default User with id = -1
+INSERT INTO public."User" (id, name, email, password, role, "createdAt") VALUES (-1, 'Default', 'default', 'default_password', 'preview', CURRENT_TIMESTAMP);
+
+-- Update Post records with empty createdBy to relate to User -1
+UPDATE public."Post" SET "createdBy" = -1 WHERE "createdBy" IS NULL
