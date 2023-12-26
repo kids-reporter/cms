f2307d2c7708a5861e73919ccf036d6bf2d33436
duidae <duidae@gmail.com>
2023-12-26 14:02:32 +0800

    fix(cms): remove migration file

--- packages/cms/migrations/20231221070854_support_ordered_relation_for_topic_and_editorpicks/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "EditorPicksSetting" ADD COLUMN     "editorPicksOfPosts_order_json" JSONB DEFAULT '[]';
-
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "relatedPosts_order_json" JSONB DEFAULT '[]';
