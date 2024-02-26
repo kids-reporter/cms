00145c2a68a0402501ed4794ea407c6233695e1a
duidae <duidae@gmail.com>
2024-02-26 10:51:29 +0800

    fix: fix example

--- packages/draft-editor/src/buttons/link.tsx
+++ packages/draft-editor/src/buttons/link.tsx
@@ -91,11 +91,15 @@ export const LinkButton = (props: {
       }}
     >
       <p>
-        ex:
         <br />
-        外部連結: https://kids.twreporter.org/article/article1#part1
+        外部連結範例:
         <br />
-        內部錨點: #part1
+        https://kids.twreporter.org/article/article1#part1
+        <br />
+        <br />
+        內部錨點範例:
+        <br />
+        #part1
       </p>
       <TextInput
         onChange={(e) => setUrlValue(e.target.value)}
