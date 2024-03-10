9681f9bbadf81d0c2d2c2e921043d386a9b98b44
duidae <duidae@gmail.com>
2024-03-10 21:47:39 +0800

    fix: fix editor interface

--- packages/draft-editor/src/index.tsx
+++ packages/draft-editor/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { RichTextEditor as _RichTextEditor } from './rich-text-editor'
+import {
+  RichTextEditor as _RichTextEditor,
+  RichTextEditorProps,
+} from './rich-text-editor'
 import buttonNames from './buttons/bt-names'
 import { editableAnnotationDecorator } from './entity-decorators/annotation'
 import { editableLinkDecorator } from './entity-decorators/link'
@@ -13,7 +16,7 @@ const decorators = [
   editableAnchorDecorator,
 ]
 
-const RichTextEditor = (props: any) => {
+const RichTextEditor = (props: RichTextEditorProps) => {
   return <_RichTextEditor decorators={decorators} {...props} />
 }
 
