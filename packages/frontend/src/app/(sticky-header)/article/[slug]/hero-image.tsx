8afb9c4047d40ceaff7cede12a111bbeba838b02
duidae <duidae@gmail.com>
2024-03-27 14:13:45 +0800

    fix: fix hero image part

--- packages/frontend/src/app/(sticky-header)/article/[slug]/hero-image.tsx
+++ packages/frontend/src/app/(sticky-header)/article/[slug]/hero-image.tsx
@@ -7,19 +7,25 @@ type HeroImageProp = {
 }
 
 export const HeroImage = (props: HeroImageProp) => {
-  const image = props.image // TODO: placeholder for missing image
-  const caption = props.caption ?? ''
+  const { image, caption } = props
+  const aspectRatio =
+    image?.imageFile?.width && image?.imageFile?.height
+      ? `${image.imageFile.width}/${image.imageFile.height}`
+      : undefined
+
   return (
-    image && (
+    image &&
+    aspectRatio && (
       <figure className="hero-image">
         <div className="image-container">
           <img
             srcSet={`${image.resized?.small} 320w, ${image.resized?.medium} 500w, ${image.resized?.large} 1000w`}
             sizes="(min-width: 1100px) 1000px, 90vw"
             src={image.resized?.medium}
+            style={{ aspectRatio: aspectRatio }}
           />
         </div>
-        <figcaption>{caption}</figcaption>
+        <figcaption>{caption ?? ''}</figcaption>
       </figure>
     )
   )
