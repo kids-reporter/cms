9acf05d6b7a5ecd6b604fb02b17cfce44c78756c
duidae <duidae@gmail.com>
2024-04-22 15:05:18 +0800

    fix: fix btn font

--- packages/frontend/src/app/home/main-header.tsx
+++ packages/frontend/src/app/home/main-header.tsx
@@ -1,14 +1,44 @@
 import Link from 'next/link'
-import StickyHeader, {
-  ContributeBtn,
-  SubscribeBtn,
-  AboutUsBtn,
-} from '@/app/components/header'
+import StickyHeader from '@/app/components/header'
 import HomeTopDetector from './home-top-detector'
 import { Navigation } from '@/app/components/navigation'
 import { SearchIcon } from '@/app/icons'
-import { SEARCH_PLACEHOLDER } from '@/app/constants'
+import { SUBSCRIBE_URL, SEARCH_PLACEHOLDER } from '@/app/constants'
 import styles from './main-header.module.css'
+
+const ContributeBtn = (
+  <Link
+    href="/about#post"
+    className="header-left__btn-1 rpjr-btn leading-6 text-sm lg:text-lg font-medium px-3"
+    style={{ marginRight: '14px', fontFamily: 'Noto Sans TC, Sans-Serif' }}
+  >
+    投稿
+  </Link>
+)
+
+const SubscribeBtn = (
+  <Link
+    href={SUBSCRIBE_URL}
+    target="_blank"
+    className="header-left__btn-1 rpjr-btn rpjr-btn-orange leading-6 text-sm lg:text-lg font-medium px-3"
+    style={{ marginRight: '15px', fontFamily: 'Noto Sans TC, Sans-Serif' }}
+  >
+    訂閱
+  </Link>
+)
+
+const AboutUsBtn = (
+  <Link
+    href="/about#us"
+    style={{ fontFamily: 'Noto Sans TC, Sans-Serif' }}
+    className="rpjr-btn rpjr-btn-red leading-6 text-sm lg:text-lg font-medium px-3"
+    aria-label="我們是誰"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    我們是誰
+  </Link>
+)
 
 export const MainHeader = () => {
   const searchInput = (
