479291e1c5aed07670dfab6f477bdf02397f4753
duidae <duidae@gmail.com>
2024-04-25 15:10:12 +0800

    fix: fix style

--- packages/frontend/src/app/(sticky-header)/error/page.tsx
+++ packages/frontend/src/app/(sticky-header)/error/page.tsx
@@ -1,12 +1,26 @@
 export default async function Error() {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div
+      style={{ width: '95vw' }}
+      className="flex flex-col justify-center items-center mb-16"
+    >
       <img
+        className="max-w-xl w-full mt-20 mb-16"
         src="./assets/images/500_error.png"
         alt="500 Internal Server Error"
       />
-      <span className="text-3xl">伺服器遭遇困難⋯⋯</span>
-      <span>請稍後或重新整理頁面。</span>
+      <div className="flex flex-col justify-center items-center gap-2.5">
+        <h1 className="text-3xl font-bold">伺服器遭遇困難⋯⋯</h1>
+        <span
+          style={{
+            fontFamily: 'var(--fontFamily)',
+            color: 'var(--paletteColor3)',
+          }}
+          className="text-lg text-center font-medium"
+        >
+          請稍後或重新整理頁面。
+        </span>
+      </div>
     </div>
   )
 }
