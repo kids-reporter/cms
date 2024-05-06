2d6b79ac0a784f32db6f6b0fb2b1e25ff09c65ff
duidae <duidae@gmail.com>
2024-05-06 11:00:49 +0800

    fix: refine a little bit

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -10,9 +10,10 @@ import CallToAction from '@/app/home/call-to-action'
 import GoToMainSite from '@/app/home/go-to-main-site'
 import { GENERAL_DESCRIPTION, POST_CONTENT_GQL, Theme } from '@/app/constants'
 import { getPostSummaries, sendGQLRequest } from '@/app/utils'
+import { isProduction } from '@/environment-variables'
 
 // Specify revalidation time for static rendering in production env
-export const revalidate = process.env.NEXT_PUBLIC_RELEASE_ENV === 'prod' ? 300 : 0
+export const revalidate = isProduction ? 300 : 0
 export const metadata: Metadata = {
   title: '少年報導者 The Reporter for Kids - 理解世界 參與未來',
   description: GENERAL_DESCRIPTION,
