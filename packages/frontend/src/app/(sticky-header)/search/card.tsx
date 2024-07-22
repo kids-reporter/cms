641ce2c7205ffa0329884832dd55572e062faacb
許守傑 Shou-Chieh Hsu (Jason) <duidae@gmail.com>
2024-07-22 17:32:45 +0800

    Revert "Revert "Revert "Revert "feat(fe): improve search result"""" (#605)
    * Revert "Revert "Revert "Revert "feat(fe): improve search result""""
    
    * fix: fix text part align
    
    * fix: fix rwd

---
+++ packages/frontend/src/app/(sticky-header)/search/card.tsx
@@ -0,0 +1,126 @@
+import Link from 'next/link'
+import { PostSummary, Loading } from '@/app/components/types'
+import { getFormattedDate } from '@/app/utils'
+import { ContentType } from '@/app/constants'
+
+const fallbackImg = '/assets/images/404.png'
+
+export type CardProp = {
+  className?: string
+  content: PostSummary & { type: ContentType; postCount: number }
+}
+
+export const Card = ({ className, content }: CardProp) => {
+  const top = (
+    <div className="flex flex-row">
+      <span
+        style={{ color: 'var(--theme-color)', lineHeight: '160%' }}
+        className="text-left font-medium md:text-base text-sm tracking-wider"
+      >
+        {content.category}
+      </span>
+      {content.publishedDate || content.postCount > 0 ? (
+        <span
+          className="md:text-base text-sm"
+          style={{ color: '#A3A3A3', letterSpacing: '0.08em' }}
+        >
+          ｜
+        </span>
+      ) : null}
+      {content.publishedDate ? (
+        <span
+          className="md:text-base text-sm"
+          style={{ color: '#A3A3A3', letterSpacing: '0.08em' }}
+        >
+          {`${getFormattedDate(content.publishedDate)}${
+            content.type === ContentType.TOPIC ? ' 最後更新·' : ''
+          }`}
+        </span>
+      ) : null}
+      {content?.postCount !== undefined && content.postCount > 0 ? (
+        <span
+          className="md:text-base text-sm"
+          style={{ color: '#A3A3A3', letterSpacing: '0.08em' }}
+        >
+          {`共 ${content.postCount} 篇文章`}
+        </span>
+      ) : null}
+    </div>
+  )
+
+  const title = (
+    <span
+      style={{
+        minHeight: 'auto',
+        display: '-webkit-box',
+        WebkitBoxOrient: 'vertical',
+        WebkitLineClamp: '1',
+        lineHeight: '160%',
+        letterSpacing: '0.08em',
+      }}
+      className="md:min-h-16 overflow-hidden not-italic font-bold md:text-2xl text-xl text-gray-900 text-left"
+    >
+      {content?.type === ContentType.TAG ? '#' : ''}
+      {content?.title}
+    </span>
+  )
+
+  const desc = (
+    <span
+      style={{
+        display: '-webkit-box',
+        WebkitBoxOrient: 'vertical',
+        WebkitLineClamp: '3',
+        lineHeight: '160%',
+      }}
+      className="overflow-hidden text-left not-italic font-medium text-base tracking-wider text-gray-900"
+    >
+      {content.desc}
+    </span>
+  )
+
+  const textPart = (
+    <div className="flex flex-col justify-start md:gap-1.5 gap-1">
+      {top}
+      {title}
+      {desc}
+    </div>
+  )
+
+  const imagePart = (
+    <div
+      style={{ aspectRatio: '16/9' }}
+      className="shrink-0 max-w-full md:h-40 h-full relative overflow-hidden rounded-2xl"
+    >
+      <img
+        style={{ borderRadius: '20px' }}
+        className={`w-full h-full object-cover align-middle hover:scale-125`}
+        src={content.image ?? fallbackImg}
+        loading={Loading.LAZY}
+      />
+      {content?.postCount !== undefined && content.postCount > 0 && (
+        <img
+          className="absolute top-2 right-2"
+          src="/assets/images/multiple_articles.png"
+          loading={Loading.LAZY}
+        />
+      )}
+    </div>
+  )
+
+  return (
+    content && (
+      <Link
+        href={content.url}
+        className={`w-full flex justify-start flex-col-reverse md:flex-row md:gap-6 md:gap-5 gap-3 bg-transparent rounded-2xl theme-${
+          content.theme
+        } ${className ?? ''}`}
+      >
+        {textPart}
+        {imagePart}
+      </Link>
+    )
+  )
+}
+
+export default Card
--- packages/frontend/src/app/(sticky-header)/search/page.tsx
+++ packages/frontend/src/app/(sticky-header)/search/page.tsx
@@ -1,15 +1,22 @@
 import errors from '@twreporter/errors'
-import { LoadMoreResults } from './results'
+import { LoadMoreResults } from './load-more-results'
+import { SearchInput } from './search-input'
 import { SearchTitle } from './styled'
 import {
   getFilteredSearchResults,
-  transferItemsToPostCards,
+  transferItemsToCards,
   defaultCount,
 } from '@/app/api/search/route'
+import { EMAIL, ContentType } from '@/app/constants'
 import { LogLevel, log } from '@/app/utils'
 
 const apiKey = process.env.SEARCH_API_KEY || ''
 const cx = process.env.SEARCH_ENGINE_ID || ''
+
+// Filtering search output: https://developers.google.com/custom-search/docs/structured_search
+const filterParams = Object.values(ContentType)
+  .map((type) => `more:pagemap:metatags-contenttype:${type}`)
+  .join(' OR ')
 
 export default async function SearchPage({
   searchParams,
@@ -25,7 +32,7 @@ export default async function SearchPage({
   let data
   try {
     data = await getFilteredSearchResults({
-      q: searchParams.q,
+      q: `${searchParams.q} ${filterParams}`,
       apiKey,
       cx,
       start: 1,
@@ -42,18 +49,37 @@ export default async function SearchPage({
     return (
       <SearchTitle>
         搜尋結果服務異常，請稍候再試。 若持續發生，煩請來信至
-        kidsnews@twreporter.org。
+        {EMAIL}。
       </SearchTitle>
     )
   }
 
+  const searchImg = (
+    <img
+      className="md:px-4 px-3"
+      src="/assets/images/search-result.png"
+      loading="lazy"
+    />
+  )
+
+  const resultCount = data?.totalResults && (
+    <p
+      style={{ letterSpacing: '0.08em', color: '#595959' }}
+      className="w-full text-left text-sm font-medium pt-4 border-t-2 border-gray-200"
+    >
+      找到 {data.totalResults} 項結果
+    </p>
+  )
+
   const cardItems = Array.isArray(data?.items)
-    ? transferItemsToPostCards(data.items)
+    ? await transferItemsToCards(data.items)
     : []
 
   return (
-    <div>
-      <SearchTitle>搜尋結果 {searchParams.q}</SearchTitle>
+    <div className="lg:max-w-4xl md:max-w-2xl max-w-full flex flex-col justify-center items-center pt-8 mx-4">
+      {searchImg}
+      <SearchInput value={searchParams.q} />
+      {resultCount}
       <LoadMoreResults
         currentCardItems={cardItems}
         nextQuery={data.nextQuery}
