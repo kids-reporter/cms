8aee232997ae17408c9192589b3f0f89df8c5b62
duidae <duidae@gmail.com>
2024-07-30 09:52:27 +0800

    fix: use defined type

--- packages/frontend/src/app/api/draft/route.ts
+++ packages/frontend/src/app/api/draft/route.ts
@@ -1,18 +1,13 @@
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
-import { PREVIEW_SECRET } from '@/app/constants'
-
-enum DraftType {
-  ARTICLE = 'article',
-  TOPIC = 'topic',
-}
+import { PREVIEW_SECRET, ContentType } from '@/app/constants'
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url)
   const secret = searchParams.get('secret')
   const type = searchParams.get('type')
   const slug = searchParams.get('slug')
-  const isValidType = type === DraftType.ARTICLE || type === DraftType.TOPIC
+  const isValidType = type === ContentType.ARTICLE || type === ContentType.TOPIC
 
   if (secret !== PREVIEW_SECRET || !isValidType || !slug) {
     return new Response('Invalid parameters.', { status: 401 })
