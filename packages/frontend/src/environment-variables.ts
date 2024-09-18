72fb146446b59f1605aebaeb94732a6fe5f3275c
許守傑 Shou-Chieh Hsu (Jason) <duidae@gmail.com>
2024-09-18 17:05:09 +0800

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Rever…"
    This reverts commit 52efc825c7586a7fc29157ff4bf28d9078dd5bb2.

--- packages/frontend/src/environment-variables.ts
+++ packages/frontend/src/environment-variables.ts
@@ -2,12 +2,14 @@ export const internalGqlEndpoint =
   process.env.INTERNAL_GQL_ENDPOINT || 'http://localhost:3001/api/graphql'
 export const gqlEndpoint =
   process.env.NEXT_PUBLIC_GQL_ENDPOINT || 'http://localhost:3001/api/graphql'
+export const previewSecretPath = process.env.PREVIEW_SECRET_PATH || ''
 export const isPreviewMode = process.env.NEXT_PUBLIC_IS_PREVIEW_MODE === 'true'
 export const isProduction = process.env.NEXT_PUBLIC_RELEASE_ENV === 'prod'
 
 export default {
   internalGqlEndpoint,
   gqlEndpoint,
+  previewSecretPath,
   isPreviewMode,
   isProduction,
 }
