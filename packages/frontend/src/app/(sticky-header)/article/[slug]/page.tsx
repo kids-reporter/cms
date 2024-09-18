42caf6162d36b774c2d2404bbf6ae89558324189
許守傑 Shou-Chieh Hsu (Jason) <duidae@gmail.com>
2024-09-18 10:30:00 +0800

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "feat(…" (#651)
    This reverts commit 7d72dbef95401e8f62a1b5099946dcea72b8473c.

--- packages/frontend/src/app/(sticky-header)/article/[slug]/page.tsx
+++ packages/frontend/src/app/(sticky-header)/article/[slug]/page.tsx
@@ -1,6 +1,4 @@
-import { promises as fs } from 'fs'
 import { Metadata } from 'next'
-import { draftMode } from 'next/headers'
 import { notFound } from 'next/navigation'
 import { TOC, TOCIndex } from './table-of-content'
 import Article from './article'
@@ -10,7 +8,6 @@ import {
   POST_CONTENT_GQL,
   OG_SUFFIX,
   ContentType,
-  PREVIEW_SECRET_PATH,
 } from '@/app/constants'
 import { sendGQLRequest, log, LogLevel } from '@/app/utils'
 
@@ -167,8 +164,18 @@ export async function generateMetadata({
   }
 }
 
-const getPost = async (slug: string) => {
-  const data = {
+export default async function PostPage({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const slug = params.slug
+  if (!slug) {
+    log(LogLevel.WARNING, 'Invalid post slug!')
+    notFound()
+  }
+
+  const postRes = await sendGQLRequest({
     query: postGQL,
     variables: {
       where: {
@@ -182,35 +189,7 @@ const getPost = async (slug: string) => {
       orderBy: [{ order: 'asc' }],
       take: topicRelatedPostsNum,
     },
-  }
-  const { isEnabled } = draftMode()
-
-  if (isEnabled) {
-    const secretValue = await fs.readFile(PREVIEW_SECRET_PATH, {
-      encoding: 'utf8',
-    })
-    return await sendGQLRequest(data, {
-      headers: {
-        Authorization: `Basic preview_${secretValue}`,
-      },
-    })
-  } else {
-    return await sendGQLRequest(data)
-  }
-}
-
-export default async function PostPage({
-  params,
-}: {
-  params: { slug: string }
-}) {
-  const slug = params.slug
-  if (!slug) {
-    log(LogLevel.WARNING, 'Invalid post slug!')
-    notFound()
-  }
-
-  const postRes = await getPost(slug)
+  })
   const post = postRes?.data?.data?.post
   if (!post) {
     log(LogLevel.WARNING, `Post not found! ${slug}`)
