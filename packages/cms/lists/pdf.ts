8b29c173db92caf5c164943ad0fc031f026085dd
nickhsine <nickhsine97753017@gmail.com>
2024-09-26 13:13:43 +0800

    refactor(cms): change pdf embed code Use mozilla/pdf.js lib to render pdf content. Old way uses <iframe> to render pdf content, but it did not work well on mobile devices. See [Asana Card](https://app.asana.com/0/1205517559975955/1208337869158726/f) for more info.

--- packages/cms/lists/pdf.ts
+++ packages/cms/lists/pdf.ts
@@ -32,18 +32,8 @@ const listConfigurations = list({
           if (typeof filename !== 'string' || filename === '') {
             return ''
           }
-          const pdfLink = `${config.googleCloudStorage.origin}/files/${filename}`
-          const downloadLink = `${pdfLink}?download=true`
-          const title = item.name
-          const code = `<iframe src="${pdfLink}#toolbar=0" width="100%" height="480" allow="autoplay" style="margin-bottom: 27px;"></iframe>
-          <div style="display: flex; align-items: center; justify-content: center; gap:11px;">
-            <span style="font-size: 16px; color: #27B5F7;">▶ ${title}</span>
-            <a href=${downloadLink} download style="text-decoration: none;">
-              <div style="color: white; background-color:#27B5F7; padding: 5px 20px; line-height: 30px; font-size: 15px; border-radius: 3px;">下載</div>
-            </a>
-          </div>
-          `
-          return code
+          const pdfURL = `${config.googleCloudStorage.origin}/files/${filename}`
+          return createEmbedCode(pdfURL, `kids-reporter-embed-pdf-${item.id}`)
         },
       }),
       ui: {
@@ -87,5 +77,68 @@ const listConfigurations = list({
 
   hooks: {},
 })
+
+function createEmbedCode(pdfURL: string, htmlId: string): string {
+  const attrName = 'data-' + htmlId
+  const tmpl = `
+<div style="padding-bottom: 60%; position: relative; overflow: scroll; width: 100%;">
+  <div id="${htmlId}" style="position: absolute; width: 100%;">
+    <div class="pdfViewer"></div>
+  </div>
+</div>
+
+<script type="module">
+  import "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.6.82/build/pdf.mjs";
+  import "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.6.82/web/pdf_viewer.mjs";
+
+  // The workerSrc property shall be specified.
+  pdfjsLib.GlobalWorkerOptions.workerSrc =
+    "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.6.82/build/pdf.worker.mjs";
+
+  const container = document.getElementById("${htmlId}");
+
+  const eventBus = new pdfjsViewer.EventBus();
+
+  const pdfViewer = new pdfjsViewer.PDFViewer({
+    container,
+    eventBus,
+  });
+
+  eventBus.on("pagesinit", function () {
+    // We can use pdfViewer now, e.g. let's change default scale.
+    pdfViewer.currentScaleValue = "page-fit";
+  });
+
+  // Loading document.
+  const loadingTask = pdfjsLib.getDocument({
+    url: "${pdfURL}"
+  });
+
+  try {
+    const pdfDocument = await loadingTask.promise;
+    // Document loaded, specifying document for the viewer
+    pdfViewer.setDocument(pdfDocument);
+  } catch(error) {
+    console.error("${pdfURL} can not be fetched.", error)
+  }
+
+  // add pdf custom styles
+  const head = document.head;
+  const fragment = document.createDocumentFragment();
+  const styleEle = document.querySelector("head > style[${attrName}]");
+
+  if (styleEle) {
+    head.removeChild(styleEle);
+  }
+
+  const newStyleEle = document.createElement("style");
+  newStyleEle.setAttribute("${attrName}", "");
+  newStyleEle.innerText = "#${htmlId} .pdfViewer{ .page { margin-left: auto; margin-right: auto; }}";
+  fragment.appendChild(newStyleEle);
+  head.appendChild(fragment);
+</script>
+  `
+  return tmpl
+}
 
 export default listConfigurations
