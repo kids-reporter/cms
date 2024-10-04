b7f511e5bf4e5084c33c44dec36ebb415475f843
許守傑 Shou-Chieh Hsu (Jason) <duidae@gmail.com>
2024-10-04 15:40:45 +0800

    Revert "test(cms): test dev for 2fa issue (#673)" (#674)
    This reverts commit b42cb0b871cc8eaa0fe70f3f1ee0e3b3fc0c17de.

--- packages/cms/lists/views/two-fa-status.tsx
+++ packages/cms/lists/views/two-fa-status.tsx
@@ -73,7 +73,7 @@ export const Field = ({ value }: FieldProps<typeof controller>) => {
               id: value['id'],
             },
             data: {
-              twoFactorAuthBypass: !value['bypass'],
+              twoFactorAuthBypass: !currentUser?.twoFactorAuth.bypass,
             },
           },
         })
