5746dd71a923cb7f1563f3c5b2742c1af684976a
duidae <duidae@gmail.com>
2024-10-22 14:46:22 +0800

    fix: fix ui

--- packages/cms/lists/views/ai-dialog.tsx
+++ packages/cms/lists/views/ai-dialog.tsx
@@ -1,30 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import copyToClipboard from 'clipboard-copy'
 import { FieldProps } from '@keystone-6/core/types'
 import { FieldLabel, FieldContainer, TextInput } from '@keystone-ui/fields'
 import { Button } from '@keystone-ui/button'
+import { Tooltip } from '@keystone-ui/tooltip'
+import { ClipboardIcon } from '@keystone-ui/icons/icons/ClipboardIcon'
 import { controller } from '@keystone-6/core/fields/types/virtual/views'
 
-const Command = styled.div`
+const Row = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 5px;
+  gap: 10px;
   margin-bottom: 5px;
 `
 
 const vendor = 'ChatGPT'
 
 export const Field = ({ value }: FieldProps<typeof controller>) => {
+  const [prompt, setPrompt] = useState<string>('')
+  const [result, setResult] = useState<string>('')
+
   const handleClick = () => {
-    console.log('handleClick')
+    console.log('prompt', prompt)
+    setResult('bullshit from ChatGPT')
   }
 
-  const handlePrompt = () => {
-    console.log('handlePrompt')
-  }
-
-  const handleGeneratedContent = () => {
-    console.log('handleGeneratedContent')
+  const handlePrompt = (event) => {
+    setPrompt(event.target.value)
   }
 
   return (
@@ -33,15 +36,26 @@ export const Field = ({ value }: FieldProps<typeof controller>) => {
         {vendor}
         {value.label}
       </FieldLabel>
-      <Command>
-        <TextInput placeholder="指令" onChange={handlePrompt} value={''} />
+      <Row>
+        <TextInput placeholder="指令" onChange={handlePrompt} value={prompt} />
         <Button onClick={handleClick}>發送</Button>
-      </Command>
-      <TextInput
-        placeholder="生成內容"
-        onChange={handleGeneratedContent}
-        value={''}
-      />
+      </Row>
+      <Row>
+        <TextInput placeholder="生成內容" value={result} />
+        <Tooltip content="Copy result">
+          {(props) => (
+            <Button
+              {...props}
+              aria-label="Copy result"
+              onClick={() => {
+                copyToClipboard(result)
+              }}
+            >
+              <ClipboardIcon size="small" />
+            </Button>
+          )}
+        </Tooltip>
+      </Row>
     </FieldContainer>
   )
 }
