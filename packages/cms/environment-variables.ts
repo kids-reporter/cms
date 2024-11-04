9acbd44f3ed8ed625983224bdb1562f149c08981
duidae <duidae@gmail.com>
2024-11-04 15:01:15 +0800

    fix: fix logging

--- packages/cms/environment-variables.ts
+++ packages/cms/environment-variables.ts
@@ -89,7 +89,7 @@ const environmentVariables = {
     serviceName: TWO_FACTOR_AUTH_SERVICE_NAME || 'KidsReporter Keystone', // the service name show in 2FA app
   },
   nodeEnv: NODE_ENV || 'development', // value could be 'development', 'production' or 'test'
-  openAIKey: OPEN_AI_KEY || '',
+  openAIKey: OPEN_AI_KEY || 'open-ai-key',
 }
 
 export default environmentVariables
