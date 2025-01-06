0c3578a9664710ff97c60f03c60eaf18d36a4168
duidae <duidae@gmail.com>
2025-01-06 10:40:30 +0800

    fix: fix style

--- packages/frontend/src/app/(sticky-header)/account/account-tabs.tsx
+++ packages/frontend/src/app/(sticky-header)/account/account-tabs.tsx
@@ -82,6 +82,43 @@ const Divider = styled.div`
   maring-bottom: 24px;
 `
 
+const Checkbox = (props: { checked: boolean; label: string }) => {
+  return (
+    <div className="inline-flex items-center">
+      <label className="flex items-center cursor-pointer relative">
+        <input
+          type="checkbox"
+          checked={props.checked}
+          className="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-slate-300 checked:bg-[#27B5F7] checked:border-[#27B5F7]"
+          id="check1"
+        />
+        <span className="absolute text-white opacity-0 peer-checked:opacity-100 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-3.5 w-3.5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="1"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </span>
+      </label>
+      <label
+        style={{ color: '#575757' }}
+        className="ms-2 text-base font-medium"
+      >
+        {props.label}
+      </label>
+    </div>
+  )
+}
+
 const grayColor = '#F8F8F8'
 
 const newsletterPreview = ''
@@ -250,32 +287,15 @@ export const AccountTabs = (props: { accoutSettings: AccountSettings }) => {
           style={{ background: grayColor }}
         >
           <SubTitle>思辨題數量</SubTitle>
-          <div className="flex flex-row">
-            <div className="flex items-center mb-4">
-              <input
-                type="checkbox"
-                value=""
-                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-              />
-              <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                無
-              </label>
-            </div>
+          <div className="flex flex-row gap-6">
+            <Checkbox checked={true} label={'無'} />
             {isQAsEnabled.map((isEnabled, index) => {
               return (
-                <div
+                <Checkbox
                   key={`qa-checkbox-${index}`}
-                  className="flex items-center mb-4"
-                >
-                  <input
-                    type="checkbox"
-                    value={isEnabled}
-                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                  />
-                  <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                    {index + 1}題
-                  </label>
-                </div>
+                  checked={isEnabled}
+                  label={`${index + 1}題`}
+                />
               )
             })}
           </div>
